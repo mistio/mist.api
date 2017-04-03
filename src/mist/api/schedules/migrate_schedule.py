daf6478c1e1f55b547cc1558ede186a77cc19504
johnnyWalnut <ikarydis0@gmail.com>
2017-04-03 19:47:42 +0300

    delete this file, it would be needed

--- src/mist/api/schedules/migrate_schedule.py
+++
@@ -1,8 +0,0 @@
-import mongoengine as me
-from mist.api.schedules.models import Schedule
-
-
-class MSchedule(me.Document):
-    """Temporary Document to hold migration status for users"""
-    schedule = me.ReferenceField(Schedule, required=True)
-    migrated = me.BooleanField(default=False)
