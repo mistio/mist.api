c2fc7641f67a1f3dbf9251c3f0ba3e197eca72d2
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-04-27 17:22:04 +0300

    Fixed dependency

--- src/mist/api/notifications/models.py
+++ src/mist/api/notifications/models.py
@@ -5,6 +5,47 @@ import mongoengine as me
 # NotificationPolicy -> NotificationRule(List)   -> NotificationOperator(List)
 #                    -> NotificationChannel(List) <-'
 
+class NotificationChannel(me.EmbeddedDocument):
+    """
+    Represents a single Notifications channel, such as
+    email, desktop, in-app etc.
+    """
+    ctype = me.StringField(max_length=20, default="")
+    cid = me.StringField(max_length=64, default="")
+
+
+class NotificationOperator(me.EmbeddedDocument):
+    """
+    Represents a single Notifications operator, which
+    essentially corresponds to an allow/block action
+    """
+    channel = me.EmbeddedDocumentField(NotificationChannel)
+    #rule = me.EmbeddedDocumentField(NotificationRule)
+    cid = me.StringField(max_length=64, default="")
+    value = me.StringField(max_length=5, required=True,
+                           choices=('ALLOW', 'BLOCK'))
+
+
+class NotificationRule(me.EmbeddedDocument):
+    """
+    Represents a single Notifications rule, which includes
+    a filter expression and a list of rules of length equal
+    to the number of channels defined in the parent policy
+    """
+    condition = me.StringField(max_length=200, required=True, default="")
+    operators = me.EmbeddedDocumentListField(NotificationOperator)
+
+    def channels_for_notification(self, notification):
+        """
+        Accepts a notification and checks its validity against
+        the rule, returning the list of channels which are allowed.
+        """
+        if self.notification_passes_rule(notification, self.condition):
+            return [op.channel for op in self.operators if op.value == 'ALLOW']
+
+    def notification_passes_rule(notification, condition):
+        pass
+
 
 class NotificationPolicy(me.EmbeddedDocument):
     """
@@ -40,48 +81,6 @@ class NotificationPolicy(me.EmbeddedDocument):
         or more channel ids.
         """
         pass
-
-
-class NotificationRule(me.EmbeddedDocument):
-    """
-    Represents a single Notifications rule, which includes
-    a filter expression and a list of rules of length equal
-    to the number of channels defined in the parent policy
-    """
-    rule = me.StringField(max_length=200, required=True, default="")
-    operators = me.EmbeddedDocumentListField(NotificationOperator)
-
-    def channels_for_notification(self, notification):
-        """
-        Accepts a notification and checks its validity against
-        the rule, returning the list of channels which are allowed.
-        """
-        if self.notification_passes_rule(notification, self.rule):
-            return [op.channel for op in self.operators if op.value == 'ALLOW']
-
-    def notification_passes_rule(notification, rule):
-        pass
-
-
-class NotificationOperator(me.EmbeddedDocument):
-    """
-    Represents a single Notifications operator, which
-    essentially corresponds to an allow/block action
-    """
-    channel = me.EmbeddedDocumentField(NotificationChannel)
-    rule = me.EmbeddedDocumentField(NotificationRule)
-    cid = me.StringField(max_length=64, default="")
-    value = me.StringField(max_length=5, required=True,
-                           choices=('ALLOW', 'BLOCK'))
-
-
-class NotificationChannel(me.EmbeddedDocument):
-    """
-    Represents a single Notifications channel, such as
-    email, desktop, in-app etc.
-    """
-    ctype = me.StringField(max_length=20, default="")
-    cid = me.StringField(max_length=64, default="")
 
 
 class Notification():
