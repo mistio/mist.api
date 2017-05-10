b636eafbcb7dc0c833e28b6cc1655be1374885e2
Dimitris Rozakis <dimrozakis@gmail.com>
2017-05-10 19:44:07 +0300

    Cleanup create api token view

--- src/mist/api/auth/views.py
+++ src/mist/api/auth/views.py
@@ -85,7 +85,6 @@ def create_token(request):
     ---
     email:
       description: User's email
-      required: true
       type: string
     password:
       description: User's password
@@ -102,82 +101,65 @@ def create_token(request):
       type: string
     """
 
-    # requesting user is the user that POSTed the function and he is the one
-    # that must provide his credentials. If the user has used
-    # su then requesting_user is the effective user.
-    session = request.environ['session']
-    requesting_user = session.get_user(effective=True)
-
     params = params_from_request(request)
     email = params.get('email', '').lower()
     password = params.get('password', '')
     api_token_name = params.get('name', '')
     org_id = params.get('org_id', '')
     ttl = params.get('ttl', 60 * 60)
-    if not email:
-        raise RequiredParameterMissingError("No email provided")
     if (isinstance(ttl, str) or isinstance(ttl, unicode)) and not ttl.isdigit():
         raise BadRequestError('Ttl must be a number greater than 0')
-    if int(ttl) < 0:
+    ttl = int(ttl)
+    if ttl < 0:
         raise BadRequestError('Ttl must be greater or equal to zero')
+    if not password:
+        raise RequiredParameterMissingError('password')
 
-    # concerned user is the user by whom the api token will be used.
-    if requesting_user is not None:
-        concerned_user = session.get_user(effective=False)
-    else:
+    try:
+        auth_context = auth_context_from_request(request)
+        user, org = auth_context.user, auth_context.org
+    except UserUnauthorizedError:
+        if not org_id:
+            raise RequiredParameterMissingError("No org_id provided")
+        if not email:
+            raise RequiredParameterMissingError("No email provided")
         try:
-            requesting_user = concerned_user = User.objects.get(email=email)
-        except me.DoesNotExist:
-            raise UserUnauthorizedError(email)
-
-    if requesting_user.status != 'confirmed' \
-            or concerned_user.status != 'confirmed':
-        raise UserUnauthorizedError()
-
-    if requesting_user.password is None or requesting_user.password == '':
-        if password:
-            raise BadRequestError('Wrong password')
-        else:
-            raise BadRequestError('Please use the GUI to set a password and '
-                                  'then retry')
-    else:
-        if not password:
-            raise BadRequestError('No password provided')
-        if not requesting_user.check_password(password):
-            raise BadRequestError('Wrong password')
-
-    org = None
-    if org_id:
+            org = Organization.objects.get(id=org_id)
+        except Organization.DoesNotExist:
+            try:
+                org = Organization.objects.get(name=org_id)
+            except Organization.DoesNotExist:
+                raise UserUnauthorizedError()
         try:
-            org = Organization.objects.get(me.Q(id=org_id) | me.Q(name=org_id))
-        except me.DoesNotExist:
-            raise BadRequestError("Invalid org id '%s'" % org_id)
-        if concerned_user not in org.members:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise UserUnauthorizedError()
+        if user not in org.members:
             raise ForbiddenError()
+
+    if user.status != 'confirmed':
+        raise UserUnauthorizedError()
+    if not user.password:
+        raise BadRequestError('Please use the GUI to set a password and retry')
+    if not user.check_password(password):
+        raise UserUnauthorizedError('Wrong password')
 
     # first check if the api token name is unique if it has been provided
     # otherwise produce a new one.
     if api_token_name:
-        token_with_name_not_exists(concerned_user, api_token_name)
-        session.name = api_token_name
+        # will raise exception if there exists valid token with given name
+        token_with_name_not_exists(user, api_token_name)
     else:
-        api_token_name = get_random_name_for_token(concerned_user)
-    api_tokens = ApiToken.objects(user_id=concerned_user.id, revoked=False)
-    tokens_list = []
-    for token in api_tokens:
-        if token.is_valid():
-            token_view = token.get_public_view()
-            if token_view['last_accessed_at'] == 'None':
-                token_view['last_accessed_at'] = 'Never'
-            tokens_list.append(token_view)
-
-    # FIXME: should call an optimized methods.list_tokens(active=True)
-    if len(tokens_list) < config.ACTIVE_APITOKEN_NUM:
+        api_token_name = get_random_name_for_token(user)
+    tokens_num = len([token for token in ApiToken.objects(user_id=user.id,
+                                                          revoked=False)
+                      if token.is_valid()])
+    if tokens_num >= config.ACTIVE_APITOKEN_NUM:
         new_api_token = ApiToken()
         new_api_token.name = api_token_name
         new_api_token.org = org
         new_api_token.ttl = ttl
-        new_api_token.set_user(concerned_user)
+        new_api_token.set_user(user)
         new_api_token.ip_address = ip_from_request(request)
         new_api_token.user_agent = request.user_agent
         new_api_token.save()
