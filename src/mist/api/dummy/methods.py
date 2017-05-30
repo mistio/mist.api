921880bd6dc2611051ffc11f95d822f648e02744
Christos Pollalis <pollalis.ch@gmail.com>
2017-05-30 14:49:50 +0300

    Remove deprecated code related to monitoring

--- src/mist/api/dummy/methods.py
+++ src/mist/api/dummy/methods.py
@@ -40,40 +40,5 @@ def filter_list_vpn_tunnels(auth_context, perm='read'):
     return {}
 
 
-def get_stats(owner, cloud_id, machine_id, start='', stop='', step='',
-              metrics=None, callback=None, tornado_async=False):
-    return
-
-
-def get_load(owner, start='', stop='', step='', tornado_callback=None):
-    return {}
-
-
-def check_monitoring(owner):
-    return {}
-
-
-def assoc_metric(owner, cloud_id, machine_id, metric_id, name='', unit=''):
-    return
-
-
-def update_metric(owner, metric_id, name="", unit=""):
-    return
-
-
-def disable_monitoring(owner, cloud_id, machine_id, no_ssh=False, job_id=''):
-    return
-
-
-def enable_monitoring(owner, cloud_id, machine_id, name='', dns_name='',
-                      public_ips=None, private_ips=None, no_ssh=False,
-                      dry=False, job_id='', plugins=None, deploy_async=True):
-    return {}
-
-
-def disable_monitoring_cloud(owner, cloud_id, no_ssh=False):
-    return
-
-
 def cross_populate_session_data(event, kwargs):
     return
