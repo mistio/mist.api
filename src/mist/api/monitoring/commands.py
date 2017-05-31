ca2bd7a078be0d88b3623b44ef240914c9924d53
Christos Pollalis <pollalis.ch@gmail.com>
2017-05-31 21:49:22 +0300

    Do not fetch Telegraf scripts quietly

--- src/mist/api/monitoring/commands.py
+++ src/mist/api/monitoring/commands.py
@@ -7,22 +7,22 @@ REPO = "https://gitlab.ops.mist.io/mistio/mist-telegraf/raw/master/scripts"
 
 
 def unix_install(machine_uuid):
-    cmd = "wget -qO- %s/install-telegraf.sh | sudo sh -s -- " % REPO
+    cmd = "wget -O- %s/install-telegraf.sh | sudo sh -s -- " % REPO
     cmd += "-m %s " % machine_uuid
     cmd += "-s %(host)s -d %(db)s" % INFLUX
     return cmd
 
 
 def unix_uninstall(machine_uuid):
-    return "wget -qO- %s/uninstall-telegraf.sh | sudo sh" % REPO
+    return "wget -O- %s/uninstall-telegraf.sh | sudo sh" % REPO
 
 
 def coreos_install(machine_uuid):
-    cmd = "wget -qO- %s/docker-telegraf.sh | sudo sh -s -- " % REPO
+    cmd = "wget -O- %s/docker-telegraf.sh | sudo sh -s -- " % REPO
     cmd += "-m %s " % machine_uuid
     cmd += "-s %(host)s -d %(db)s" % INFLUX
     return cmd
 
 
 def coreos_uninstall(machine_uuid):
-    return "wget -qO- %s/docker-telegraf.sh | sudo sh -s -- -k" % REPO
+    return "wget -O- %s/docker-telegraf.sh | sudo sh -s -- -k" % REPO
