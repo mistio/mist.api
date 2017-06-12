492869a1bac68ed01841139e1845d0472059201d
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-06-12 21:55:45 +0300

    conditional import of sendgrid

--- src/mist/api/notifications/channels.py
+++ src/mist/api/notifications/channels.py
@@ -1,11 +1,4 @@
 
-from sendgrid.helpers.mail import (Email,
-                                   Mail,
-                                   Personalization,
-                                   Content,
-                                   Substitution)
-import sendgrid
-
 from mist.api import config
 
 
@@ -22,42 +15,64 @@ class BaseChannel():
         pass
 
 
-class WeeklyReportsChannel(BaseChannel):
+class EmailReportsChannel(BaseChannel):
     '''
-    Sendgrid (email) channel for weekly reports
+    Email channel for reports.
+    Tries to send using Sendgrid, if credentials are available
+    in config, otherwise sends email using SMTP.
     '''
-
-    sg_instance = sendgrid.SendGridAPIClient(
-        apikey=config.SENDGRID_REPORTING_KEY)
 
     def send(self, notification):
+        '''
+        Accepts a notification and sends an email using included data.
+        If SENDGRID_REPORTING_KEY and EMAIL_REPORT_SENDER are available
+        in config, it uses Sendgrid to deliver the email. Otherwise, it
+        uses plain SMTP through send_email()
+        '''
         user = notification["user"]
 
-        mail = Mail()
-        mail.from_email = Email(config.EMAIL_REPORT_SENDER, "Mist.io Reports")
-        personalization = Personalization()
-        email = Email(notification.get("email", user.email),
-                      notification.get("full_name", user.get_nice_name()))
-        personalization.add_to(email)
-        personalization.subject = notification["subject"]
-        sub1 = Substitution("%name%", notification.get(
-            "name", user.first_name or user.get_nice_name()))
-        personalization.add_substitution(sub1)
-        if "unsub_link" in notification:
-            sub2 = Substitution("%nsub%", notification["unsub_link"])
-            personalization.add_substitution(sub2)
-        mail.add_personalization(personalization)
+        to = notification.get("email", user.email)
+        full_name = notification.get("full_name", user.get_nice_name())
+        first_name = notification.get(
+                "name", user.first_name or user.get_nice_name())
 
-        mail.add_content(Content("text/plain", notification["body"]))
-        if "html_body" in notification:
-            mail.add_content(Content("text/html", notification["html_body"]))
+        if (hasattr(config, "SENDGRID_REPORTING_KEY") and
+            hasattr(config, "EMAIL_REPORT_SENDER")):
+            from sendgrid.helpers.mail import (Email,
+                                   Mail,
+                                   Personalization,
+                                   Content,
+                                   Substitution)
+            import sendgrid
 
-        mdict = mail.get()
-        try:
-            return self.sg_instance.client.mail.send.post(request_body=mdict)
-        except Exception as exc:
-            print str(exc)
-            print exc.read()
+            self.sg_instance = sendgrid.SendGridAPIClient(
+                apikey=config.SENDGRID_REPORTING_KEY)
+
+            mail = Mail()
+            mail.from_email = Email(config.EMAIL_REPORT_SENDER, "Mist.io Reports")
+            personalization = Personalization()
+            personalization.add_to(Email(to, full_name))
+            personalization.subject = notification["subject"]
+            sub1 = Substitution("%name%", first_name)
+            personalization.add_substitution(sub1)
+            if "unsub_link" in notification:
+                sub2 = Substitution("%nsub%", notification["unsub_link"])
+                personalization.add_substitution(sub2)
+            mail.add_personalization(personalization)
+
+            mail.add_content(Content("text/plain", notification["body"]))
+            if "html_body" in notification:
+                mail.add_content(Content("text/html", notification["html_body"]))
+
+            mdict = mail.get()
+            try:
+                return self.sg_instance.client.mail.send.post(request_body=mdict)
+            except Exception as exc:
+                print str(exc)
+                print exc.read()
+        else:
+            send_email(subject, notification["body"], 
+                [to], sender="config.EMAIL_REPORT_SENDER")
 
 
 class StdoutChannel(BaseChannel):
@@ -79,6 +94,6 @@ def channel_instance_with_name(name):
     '''
     if name == 'stdout':
         return StdoutChannel()
-    elif name == 'weekly_reports':
-        return WeeklyReportsChannel()
+    elif name == 'email_reports':
+        return EmailReportsChannel()
     return None
