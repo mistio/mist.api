82fbd565b8fe4c4b887c5de4541d2c4e693ff8db
johnnyWalnut <ikarydis0@gmail.com>
2017-06-15 15:53:16 +0200

    add new machine_type, refers to swarm-nodes

--- src/mist/api/machines/models.py
+++ src/mist/api/machines/models.py
@@ -141,7 +141,8 @@ class Machine(me.Document):
                                     'error', 'paused', 'reconfiguring'))
     machine_type = me.StringField(default='machine',
                                   choices=('machine', 'vm', 'container',
-                                           'hypervisor', 'container-host'))
+                                           'hypervisor', 'container-host',
+                                           'swarm-node'))
     parent = me.ReferenceField('Machine', required=False)
 
     # We should think this through a bit.
