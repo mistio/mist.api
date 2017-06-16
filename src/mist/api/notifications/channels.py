2b5f78173e53699bef88b57cf895ff4b9663c721
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-06-16 19:01:57 +0300

    update notifications class, add inapp channel

--- src/mist/api/notifications/channels.py
+++ src/mist/api/notifications/channels.py
@@ -1,13 +1,13 @@
 
 from mist.api import config
 from mist.api.helpers import send_email
+from mist.api.helpers import trigger_session_update
 
 
 class BaseChannel():
     '''
     Represents a notification channel
     '''
-
     def send(self, notification):
         '''
         Accepts a notification and sends it using the
@@ -22,7 +22,6 @@ class EmailReportsChannel(BaseChannel):
     Tries to send using Sendgrid, if credentials are available
     in config, otherwise sends email using SMTP.
     '''
-
     def send(self, notification):
         '''
         Accepts a notification and sends an email using included data.
@@ -30,11 +29,11 @@ class EmailReportsChannel(BaseChannel):
         in config, it uses Sendgrid to deliver the email. Otherwise, it
         uses plain SMTP through send_email()
         '''
-        user = notification["user"]
+        user = notification.user
 
-        to = notification.get("email", user.email)
-        full_name = notification.get("full_name", user.get_nice_name())
-        first_name = notification.get(
+        to = getattr(notification, "email", user.email)
+        full_name = getattr(notification, "full_name", user.get_nice_name())
+        first_name = getattr(notification,
             "name", user.first_name or user.get_nice_name())
 
         if (hasattr(config, "SENDGRID_REPORTING_KEY") and
@@ -55,20 +54,20 @@ class EmailReportsChannel(BaseChannel):
                 "Mist.io Reports")
             personalization = Personalization()
             personalization.add_to(Email(to, full_name))
-            personalization.subject = notification["subject"]
+            personalization.subject = notification.subject
             sub1 = Substitution("%name%", first_name)
             personalization.add_substitution(sub1)
             if "unsub_link" in notification:
-                sub2 = Substitution("%nsub%", notification["unsub_link"])
+                sub2 = Substitution("%nsub%", notification.unsub_link)
                 personalization.add_substitution(sub2)
             mail.add_personalization(personalization)
 
-            mail.add_content(Content("text/plain", notification["body"]))
+            mail.add_content(Content("text/plain", notification.body))
             if "html_body" in notification:
                 mail.add_content(
                     Content(
                         "text/html",
-                        notification["html_body"]))
+                        notification.html_body))
 
             mdict = mail.get()
             try:
@@ -78,20 +77,29 @@ class EmailReportsChannel(BaseChannel):
                 print str(exc)
                 print exc.read()
         else:
-            send_email(notification["subject"], notification["body"],
+            send_email(notification.subject, notification.body,
                        [to], sender="config.EMAIL_REPORT_SENDER")
+
+
+class InAppChannel(BaseChannel):
+    '''
+    In-app Notifications channel
+    Saves notification and triggers a session update
+    '''
+    def send(self, notification):
+        notification.save()
+        trigger_session_update('notifications', notification.user)
 
 
 class StdoutChannel(BaseChannel):
     '''
     Stdout channel, mainly for testing/debugging
     '''
-
     def send(self, notification):
-        print notification["subject"]
+        print notification.subject
         if "summary" in notification:
-            print notification["summary"]
-        print notification["body"]
+            print notification.summary
+        print notification.body
 
 
 def channel_instance_with_name(name):
