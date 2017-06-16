3b6918b56ddc676543c48b31fe788cc60120c132
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-06-16 19:00:48 +0300

    add notification class

--- src/mist/api/notifications/models.py
+++ src/mist/api/notifications/models.py
@@ -24,9 +24,38 @@ class UserNotificationPolicy(me.Document):
     organization = me.ReferenceField(Organization, required=True)
 
     def notification_allowed(self, notification):
+        '''
+        Accepts a notification and returns a boolean indicating
+        whether it is allowed or has been blocked by the user
+        '''
         for rule in self.rules:
             # TODO: here eventually check for source as well
-            if (rule.source == notification["source"] and
+            if (rule.source == notification.source and
                     rule.value == 'BLOCK'):
                 return False
         return True
+
+
+class Notification(me.Document):
+    '''
+    Represents a notification associated with a 
+    user-organization pair
+    '''
+    created_date = me.DateTimeField()
+    expiry_date = me.DateTimeField()
+
+    user = me.ReferenceField(User, required=True)
+    organization = me.ReferenceField(Organization, required=True)
+
+    summary = me.StringField(max_length=512, required=False, default="")
+    subject = me.StringField(max_length=256, required=False, default="")
+    body = me.StringField(required=True, default="")
+    html_body = me.StringField(required=False, default="")
+
+    source = me.StringField(max_length=64, required=True, default="")
+    channel = me.StringField(max_length=64, required=True, default="")
+
+    email = me.EmailField(required=False)
+    ubsub_link = me.URLField(required=False)
+
+
