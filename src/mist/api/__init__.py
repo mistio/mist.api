34de95d36f67e36d4748bc2bbae6c4e71c947090
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-06-28 20:25:32 +0300

    register dismiss endpoint

--- src/mist/api/__init__.py
+++ src/mist/api/__init__.py
@@ -290,6 +290,9 @@ def add_routes(configurator):
     configurator.add_route('api_v1_job', '/api/v1/jobs/{job_id}')
     configurator.add_route('api_v1_story', '/api/v1/stories/{story_id}')
 
+    # Notifications
+    configurator.add_route('api_v1_dismiss_notification', '/api/v1/notifications/dismiss')
+
     configurator.add_route('user_invitations', '/user_invitations')
 
     configurator.add_route('su', '/su')
