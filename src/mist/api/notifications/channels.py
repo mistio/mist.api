768c5e21218a08c24300bc75b9cf65a8cfc7dd78
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-07-10 12:19:18 +0300

    change dissmiss notification endpoint to use DELETE, fix duplicate notifications being sent

--- src/mist/api/notifications/channels.py
+++ src/mist/api/notifications/channels.py
@@ -2,6 +2,7 @@
 from mist.api import config
 from mist.api.helpers import send_email, amqp_publish_user
 
+from models import Notification
 
 class BaseChannel():
     '''
@@ -89,10 +90,11 @@ class InAppChannel(BaseChannel):
     '''
 
     def send(self, notification):
-        notification.save()
-        amqp_publish_user(notification.organization,
-                          routing_key='notification',
-                          data=notification.to_json())
+        if not Notification.objects(id=notification.id) and notification.dismissed == False:
+            notification.save()
+            amqp_publish_user(notification.organization,
+                              routing_key='notification',
+                              data=notification.to_json())
 
 
 class StdoutChannel(BaseChannel):
