93ec27d41da5b64d9d8645a402a70cc1c0b8992d
Yannis Chatzikonstantinou <yconst@gmail.com>
2017-07-11 14:52:38 +0300

    add kind and resource fields to models

--- src/mist/api/notifications/models.py
+++ src/mist/api/notifications/models.py
@@ -52,20 +52,26 @@ class Notification(me.Document):
     user = me.ReferenceField(User, required=True)
     organization = me.ReferenceField(Organization, required=True)
 
+    # content fields
     summary = me.StringField(max_length=512, required=False, default="")
     subject = me.StringField(max_length=256, required=False, default="")
     body = me.StringField(required=True, default="")
     html_body = me.StringField(required=False, default="")
 
+    # source fields
     source = me.StringField(max_length=64, required=True, default="")
     channel = me.StringField(max_length=64, required=True, default="")
 
+    # resource and action fields
+    kind = me.StringField(required=True, default="")
+    resource = me.GenericReferenceField(required=False)
+    action_link = me.URLField(required=False)
+
+    # email-specific fields
     email = me.EmailField(required=False)
     unsub_link = me.URLField(required=False)
 
-    # resource
-    action_link = me.URLField(required=False)
-
+    # user action fields
     viewed = me.BooleanField(required=True, default=False)
     dismissed = me.BooleanField(required=True, default=False)
 
