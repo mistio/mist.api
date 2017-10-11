7b5107263e8300c6047f62a8f96a330e63e9efed
nVlast <nikosvlast@gmail.com>
2017-10-11 18:30:05 +0300

    add function to patch docstrings after parsing

--- scripts/generate_api_spec.py
+++ scripts/generate_api_spec.py
@@ -16,6 +16,65 @@ import mist.api
 BASE_FILE_PATH = os.path.join(this_dir,'base.yml')
 OAS_FILE_PATH = os.path.join(this_dir,'spec.yml')
 
+def patch_operation(operation):
+    ret = {}
+    if 'responses' in operation.keys():
+        ret['responses'] = operation['responses']
+    else:
+        ret['responses'] = { '200': { 'description': 'Foo' } }
+
+    if 'parameters' in operation.keys():
+        ret['parameters'] = operation['parameters']
+    else:
+        params = []
+        for key in list(set(operation.keys()) - {'parameters','requestBody','responses','description'}):
+            if 'in' in operation[key].keys():
+                p = {}
+                p['name'] = key
+                p['schema'] = {}
+                for k in operation[key].keys():
+                    if k in ['type','enum','default']:
+                        p['schema'][k] = operation[key][k]
+                    else:
+                        p[k] = operation[key][k]
+                params.append(p)
+        if params:
+            ret['parameters'] = params
+
+    if 'requestBody' in operation.keys():
+        ret['requestBody'] = operation['requestBody']
+    else:
+        reqB = {\
+                 'description': 'Description',\
+                 'required': True,\
+                 'content': {\
+                    'application/json': {\
+                                    'schema': {\
+                                        'type': 'object',\
+                                        'properties': {},\
+                                              }\
+                                        }\
+                            }\
+                }
+        properties = {}
+        required = []
+        for key in list(set(operation.keys()) - {'parameters','requestBody','responses','description'}):
+            if not 'in' in operation[key].keys():
+                p = { key: {} }
+                for k in operation[key].keys():
+                    if k != 'required':
+                        p[key][k] = operation[key][k]
+                properties.update(p)
+                if 'required' in operation[key] and operation[key]['required']:
+                    required.append(key)
+        if properties:
+            reqB['content']['application/json']['schema']['properties'] = properties
+            if required:
+                reqB['content']['application/json']['schema']['required'] = required
+            ret['requestBody'] = reqB
+
+    return ret
+
 def docstring_to_object(docstring):
     if not docstring:
         return {}
@@ -23,15 +82,10 @@ def docstring_to_object(docstring):
     operation = {}
     tokens = docstring.split('---')
     if len(tokens) > 1:
-        # sanitization
         operation = yaml.safe_load(tokens[1]) or {}
 
     description = re.sub(r'\s+',r' ',tokens[0]).strip()
     operation['description'] = description
-
-    # patch 'responses' section where it is not filled yet
-    #if not 'responses' in operation:
-    #    operation['responses'] = { '200': { 'description': 'Foo' } }
 
     return operation
 
@@ -55,13 +109,15 @@ def main():
     for path, method, operation in routes:
         if not path in paths:
             paths[path] = {}
-        paths[path][method] = operation
+        paths[path][method] = patch_operation(operation)
 
     with open(BASE_FILE_PATH,'r') as f:
         openapi = yaml.safe_load(f.read())
         openapi['paths'] = paths
     with open(OAS_FILE_PATH,'w') as f:
-        yaml.dump(openapi,f,default_flow_style=False)
+        noalias_dumper = yaml.dumper.SafeDumper
+        noalias_dumper.ignore_aliases = lambda self, data: True
+        yaml.dump(openapi,f,default_flow_style=False,Dumper=noalias_dumper)
 
 if __name__ == '__main__':
     main()
