00d8404f1d4bbfe1058b1f800c3b15cec358fa22
spiros <spirostza77@hotmail.com>
2018-01-05 13:32:28 +0100

    Poller task for list_networks

--- src/mist/api/poller/tasks.py
+++ src/mist/api/poller/tasks.py
@@ -52,6 +52,17 @@ def list_images(schedule_id):
 
 
 @app.task(time_limit=60, soft_time_limit=55)
+def list_networks(schedule_id):
+    """Perform list networks. Cloud controller stores results in mongodb."""
+
+    # Fetch schedule and cloud from database.
+    # FIXME: resolve circular deps error
+    from mist.api.poller.models import ListNetworksPollingSchedule
+    sched = ListNetworksPollingSchedule.objects.get(id=schedule_id)
+    sched.cloud.ctl.network.list_networks(persist=False)
+
+
+@app.task(time_limit=60, soft_time_limit=55)
 def list_zones(schedule_id):
     """Perform list zones. Cloud controller stores results in mongodb."""
 
