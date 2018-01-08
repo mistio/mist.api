af5500691eed6ce1605441b085a5568148ce5dcf
spiros <spirostza77@hotmail.com>
2018-01-08 17:54:40 +0100

    Do not create CLoudImage object on machine creation

--- src/mist/api/machines/methods.py
+++ src/mist/api/machines/methods.py
@@ -348,8 +348,6 @@ def create_machine(owner, cloud_id, key_id, machine_name, location_id,
         # list_machines
         try:
 
-            image = CloudImage(image_id=image_id, cloud_provider=conn.type,
-                               name=image_name).save()
             machine = Machine(cloud=cloud, machine_id=node.id,
                               image=image).save()
             # Since this is the first time the new Machine object is persisted
