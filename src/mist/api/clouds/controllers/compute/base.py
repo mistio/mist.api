83349a65af69270b4c9817d58fdb1ddd6c73c344
spiros <spirostza77@hotmail.com>
2018-01-12 16:40:13 +0100

    Return the location_id as id

--- src/mist/api/clouds/controllers/compute/base.py
+++ src/mist/api/clouds/controllers/compute/base.py
@@ -799,7 +799,7 @@ class BaseComputeController(BaseController):
         task = PeriodicTaskInfo.get_or_add(task_key)
         try:
             with task.task_runner(persist=persist):
-                cached_locations = {'%s' % l.id: l.as_dict()
+                cached_locations = {'%s' % l.location_id: l.as_dict()
                                     for l in self.list_cached_locations()}
                 locations = self._list_locations()
         except PeriodicTaskThresholdExceeded:
@@ -817,7 +817,7 @@ class BaseComputeController(BaseController):
                                         'locations': locations})
             else:
                 # Publish patches to rabbitmq.
-                new_locations = {'%s' % l.id: l.as_dict()
+                new_locations = {'%s' % l.location_id: l.as_dict()
                                  for l in locations}
                 patch = jsonpatch.JsonPatch.from_diff(cached_locations,
                                                       new_locations).patch
