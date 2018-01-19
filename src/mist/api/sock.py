b310a88f99b34700dc27a7708e7ba6ff1b23f995
spiros <spirostza77@hotmail.com>
2018-01-19 18:21:26 +0100

    Add patch_sizes routing_key

--- src/mist/api/sock.py
+++ src/mist/api/sock.py
@@ -586,6 +586,18 @@ class MainConnection(MistConnection):
             if patch:
                 self.send('patch_model', patch)
 
+        elif routing_key == 'patch_sizes':
+            cloud_id = result['cloud_id']
+            patch = result['patch']
+
+            # remove '/'
+            for line in patch:
+                size_id = line['path'][1:]
+                line['path'] = '/clouds/%s/sizes/%s' % (cloud_id,
+                                                        size_id)
+            if patch:
+                self.send('patch_model', patch)
+
     def on_close(self, stale=False):
         if not self.closed:
             kwargs = {}
