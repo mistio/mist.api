923bc66463d1241c07e5a6cd5f5c4c2cc7a13f90
commixon <commixon@gmail.com>
2018-01-23 13:04:15 +0200

    Fix flake8 double-line

--- src/mist/api/clouds/controllers/network/controllers.py
+++ src/mist/api/clouds/controllers/network/controllers.py
@@ -169,6 +169,7 @@ class OpenStackNetworkController(BaseNetworkController):
     def _delete_subnet(self, subnet, libcloud_subnet):
         self.cloud.ctl.compute.connection.ex_delete_subnet(libcloud_subnet.id)
 
+
 class LibvirtNetworkController(BaseNetworkController):
 
     def _list_networks__postparse_network(self, network, libcloud_network):
