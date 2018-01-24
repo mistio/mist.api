5628392d3154d2327868f6639f1904b2b683df6f
spiros <spirostza77@hotmail.com>
2018-01-24 21:32:58 +0100

    list_zones poller task

--- src/mist/api/poller/tasks.py
+++ src/mist/api/poller/tasks.py
@@ -49,6 +49,15 @@ def list_locations(schedule_id):
     sched.cloud.ctl.compute.list_locations(persist=False)
 
 
+@app.task(time_limit=60, soft_time_limit=55)
+def list_zones(schedule_id):
+    """Perform list zones. Cloud controller stores results in mongodb."""
+
+    from mist.api.poller.models import ListZonesPollingSchedule
+    sched = ListZonesPollingSchedule.objects.get(id=schedule_id)
+    sched.cloud.dns.compute.list_zones(persist=False)
+
+
 @app.task(time_limit=45, soft_time_limit=40)
 def ping_probe(schedule_id):
     """Perform ping probe"""
