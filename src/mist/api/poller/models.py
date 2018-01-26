d4a2f13d13774993bf11b859674103008352e6cb
spiros <spirostza77@hotmail.com>
2018-01-26 20:45:40 +0100

    Override interval property

--- src/mist/api/poller/models.py
+++ src/mist/api/poller/models.py
@@ -242,6 +242,10 @@ class ListLocationsPollingSchedule(CloudPollingSchedule):
 
     task = 'mist.api.poller.tasks.list_locations'
 
+    @property
+    def interval(self):
+        return super(PollingSchedule, self).interval
+
 
 class MachinePollingSchedule(PollingSchedule):
 
