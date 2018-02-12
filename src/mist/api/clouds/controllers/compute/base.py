3cf6d42d60e98ed168c2f18f1dc0d7f255d19d8d
Dimitris Moraitis <dimo@mist.io>
2018-02-12 16:15:10 +0200

    Cast DigitalOcean image_id to str

--- src/mist/api/clouds/controllers/compute/base.py
+++ src/mist/api/clouds/controllers/compute/base.py
@@ -329,7 +329,7 @@ class BaseComputeController(BaseController):
             # Get misc libcloud metadata.
             image_id = ''
             if isinstance(node.extra.get('image'), dict):
-                image_id = node.extra.get('image').get('id')
+                image_id = str(node.extra.get('image').get('id'))
 
             if not image_id:
                 image_id = str(node.image or node.extra.get('imageId') or
