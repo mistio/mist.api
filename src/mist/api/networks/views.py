8c7020f14e495088c1e093ad3ca79d7213b24b9c
spiros <spirostza77@hotmail.com>
2018-02-23 14:31:45 +0100

    list_subnets completed

--- src/mist/api/networks/views.py
+++ src/mist/api/networks/views.py
@@ -144,6 +144,48 @@ def delete_network(request):
     return OK
 
 
+@view_config(route_name='api_v1_subnets', request_method='GET',
+             renderer='json')
+def list_subnets(request):
+    """
+    List subnets of a cloud
+    Currently supports the EC2, GCE and OpenStack clouds.
+    For other providers this returns an empty list.
+    READ permission required on cloud.
+    ---
+    cloud:
+      in: path
+      required: true
+      type: string
+    network_id:
+      in: path
+      required: true
+      description: The DB ID of the network whose subnets will be returned
+      type: string
+    """
+    cloud_id = request.matchdict['cloud']
+    network_id = request.matchdict['network']
+    auth_context = auth_context_from_request(request)
+    auth_context.check_perm("cloud", "read", cloud_id)
+
+    try:
+        cloud = Cloud.objects.get(owner=auth_context.owner, id=cloud_id)
+    except Cloud.DoesNotExist:
+        raise CloudNotFoundError
+
+    if not hasattr(cloud.ctl, 'network'):
+        return []
+
+    try:
+        network = Network.objects.get(cloud=cloud, id=network_id)
+    except Network.DoesNotExist:
+        raise NetworkNotFoundError
+
+    subnets = cloud.ctl.network.list_subnets(network=network)
+
+    return subnets
+
+
 @view_config(route_name='api_v1_network', request_method='POST')
 def associate_ip(request):
     """
