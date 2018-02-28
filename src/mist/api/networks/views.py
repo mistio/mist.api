cae7ab1ab5d1718272f0f73908d4cd170c4370ec
Christos Pollalis <pollalis.ch@gmail.com>
2018-02-28 17:53:58 +0200

    Move network-related code under networks/
    Cleaned-up top-level views.py and methods.py
    
    Implemented networks.methods:filter_list_networks to apply RBAC
    on networks' and subnets' lists

--- src/mist/api/networks/views.py
+++ src/mist/api/networks/views.py
@@ -1,19 +1,23 @@
 import mongoengine as me
 from pyramid.response import Response
 
-import mist.api.networks.methods as methods
-
 from mist.api.clouds.models import Cloud
 from mist.api.machines.models import Machine
-from mist.api.networks.models import Network
+from mist.api.networks.models import Network, Subnet
+from mist.api.networks.models import NETWORKS, SUBNETS
+from mist.api.networks.methods import filter_list_networks
 
+from mist.api.tag.methods import add_tags_to_resource
 from mist.api.auth.methods import auth_context_from_request
 
 from mist.api.exceptions import CloudNotFoundError
+from mist.api.exceptions import SubnetNotFoundError
 from mist.api.exceptions import RequiredParameterMissingError
 from mist.api.exceptions import PolicyUnauthorizedError, NetworkNotFoundError
 
 from mist.api.helpers import params_from_request, view_config
+from mist.api.helpers import trigger_session_update
+
 
 OK = Response("OK", 200)
 
@@ -24,10 +28,9 @@ def list_networks(request):
     """
     Tags: networks
     ---
-    Lists networks of a cloud.
-    Currently supports the EC2, GCE and OpenStack providers.
-    For other providers this returns an empty list.
-    READ permission required on cloud.
+    List the networks of a cloud
+
+    READ permission required on cloud, networks, and subnets
     ---
     parameters:
     - name: cloud
@@ -38,16 +41,7 @@ def list_networks(request):
     """
     cloud_id = request.matchdict['cloud']
     auth_context = auth_context_from_request(request)
-    auth_context.check_perm("cloud", "read", cloud_id)
-
-    try:
-        Cloud.objects.get(owner=auth_context.owner, id=cloud_id)
-    except me.DoesNotExist:
-        raise CloudNotFoundError
-
-    networks = methods.list_networks(auth_context.owner, cloud_id)
-
-    return networks
+    return filter_list_networks(auth_context, cloud_id)
 
 
 @view_config(route_name='api_v1_networks',
@@ -56,10 +50,15 @@ def create_network(request):
     """
     Tags: networks
     ---
-    Creates a new network. If subnet dict is specified,
-    after creating the network it will use the new
-    network's id to create a subnet.
-    CREATE_RESOURCES permission required on cloud.
+    Create a new network
+
+    If subnet parameters are specified, they will be used to create a new
+    subnet in the newly created network.
+
+    ADD permission required on network
+    ADD permission required on subnet
+    READ permission required on cloud
+    CREATE_RESOURCES permission required on cloud
     ---
     parameters:
     - name: cloud_id
@@ -94,32 +93,52 @@ def create_network(request):
     if not network_params:
         raise RequiredParameterMissingError('network')
 
-    # TODO
-    if not auth_context.is_owner():
-        raise PolicyUnauthorizedError()
+    if subnet_params and not subnet_params.get('cidr'):
+        raise RequiredParameterMissingError('cidr')
+
+    auth_context.check_perm("cloud", "read", cloud_id)
+    auth_context.check_perm("cloud", "create_resources", cloud_id)
+    tags = auth_context.check_perm("network", "add", None) or {}
 
     try:
         cloud = Cloud.objects.get(owner=auth_context.owner, id=cloud_id)
     except me.DoesNotExist:
-        raise CloudNotFoundError
+        raise CloudNotFoundError()
 
-    network = methods.create_network(auth_context.owner, cloud, network_params)
+    # Is network support available?
+    if not hasattr(cloud.ctl, 'network'):
+        raise NotImplementedError()
+
+    # Create a DB document for the new network and call libcloud to declare
+    # it on the cloud provider
+    network = NETWORKS[cloud.ctl.provider].add(cloud=cloud, **network_params)
     network_dict = network.as_dict()
+    if tags:
+        add_tags_to_resource(auth_context.owner, network, tags)
 
-    # Bundling Subnet creation in this call because it is required
-    #  for backwards compatibility with the current UI
+    # Bundling Subnet creation in this call because it is required for
+    # backwards compatibility with the current UI  # FIXME
     if subnet_params:
+        tags = auth_context.check_perm("subnet", "add", None) or {}
         try:
-            subnet = methods.create_subnet(auth_context.owner, cloud,
-                                           network, subnet_params)
+            # Create a DB document for the new subnet and call libcloud to
+            # declare it on the cloud provider
+            subnet = SUBNETS[cloud.ctl.provider].add(network=network,
+                                                     **subnet_params)
         except Exception as exc:
-            # Cleaning up the network object in case subnet creation
-            #  fails for any reason
+            # Cleaning up the network object in case subnet creation fails
+            # for any reason
             network.ctl.delete()
             raise exc
+        if tags:
+            add_tags_to_resource(auth_context.owner, subnet, tags)
+
         network_dict['subnet'] = subnet.as_dict()
 
-    return network.as_dict()
+    # Schedule a UI update
+    trigger_session_update(auth_context.owner, ['clouds'])
+
+    return network_dict
 
 
 @view_config(route_name='api_v1_network', request_method='DELETE')
@@ -127,8 +146,11 @@ def delete_network(request):
     """
     Tags: networks
     ---
-    Deletes a network.
-    CREATE_RESOURCES permission required on cloud.
+    Delete a network and all corresponding subnets
+
+    READ permission required on cloud
+    READ permission required on network
+    REMOVE permission required on network
     ---
     parameters:
     - name: cloud_id
@@ -147,21 +169,192 @@ def delete_network(request):
 
     auth_context = auth_context_from_request(request)
 
-    # TODO
-    if not auth_context.is_owner():
-        raise PolicyUnauthorizedError()
+    # SEC
+    auth_context.check_perm('cloud', 'read', cloud_id)
+    auth_context.check_perm('network', 'read', network_id)
+    auth_context.check_perm('network', 'remove', network_id)
 
     try:
         cloud = Cloud.objects.get(id=cloud_id, owner=auth_context.owner)
     except Cloud.DoesNotExist:
-        raise CloudNotFoundError
+        raise CloudNotFoundError()
     try:
         network = Network.objects.get(id=network_id, cloud=cloud)
     except me.DoesNotExist:
-        raise NetworkNotFoundError
+        raise NetworkNotFoundError()
 
-    methods.delete_network(auth_context.owner, network)
+    # Delete the network
+    network.ctl.delete()
 
+    # Schedule a UI update
+    trigger_session_update(auth_context.owner, ['clouds'])
+
+    return OK
+
+
+@view_config(route_name='api_v1_subnets', request_method='GET',
+             renderer='json')
+def list_subnets(request):
+    """
+    Tags: networks
+    ---
+    List the subnets of a network
+
+    READ permission required on cloud
+    READ permission required on network
+    READ permission required on subnets
+    ---
+    cloud:
+      in: path
+      required: true
+      type: string
+    network_id:
+      in: path
+      required: true
+      description: The DB ID of the network whose subnets will be returned
+      type: string
+    """
+    cloud_id = request.matchdict['cloud']
+    network_id = request.matchdict['network']
+
+    auth_context = auth_context_from_request(request)
+
+    # SEC
+    auth_context.check_perm('cloud', 'read', cloud_id)
+    auth_context.check_perm('network', 'read', network_id)
+
+    try:
+        cloud = Cloud.objects.get(owner=auth_context.owner, id=cloud_id)
+    except Cloud.DoesNotExist:
+        raise CloudNotFoundError()
+    try:
+        network = Network.objects.get(cloud=cloud, id=network_id)
+    except Network.DoesNotExist:
+        raise NetworkNotFoundError()
+
+    subnets = []
+    for subnet in network.ctl.list_subnets():
+        try:
+            auth_context.check_perm('subnet', 'read', subnet.id)
+        except PolicyUnauthorizedError:
+            pass
+        else:
+            subnets.append(subnet.as_dict())
+    return subnets
+
+
+@view_config(route_name='api_v1_subnets', request_method='POST',
+             renderer='json')
+def create_subnet(request):
+    """
+    Tags: networks
+    ---
+    Create subnet in a given network on a cloud
+
+    ADD permission required on subnet
+    READ permission required on cloud
+    READ permission required on network
+    CREATE_SUBNETS permission required on network
+    CREATE_RESOURCES permission required on cloud
+    ---
+    cloud_id:
+      in: path
+      required: true
+      description: The Cloud ID
+      type: string
+    network_id:
+      in: path
+      required: true
+      description: The ID of the Network that will contain the new subnet
+      type: string
+    subnet:
+      required: true
+      type: object
+    """
+    cloud_id = request.matchdict['cloud']
+    network_id = request.matchdict['network']
+
+    params = params_from_request(request)
+
+    auth_context = auth_context_from_request(request)
+
+    # SEC
+    auth_context.check_perm('cloud', 'read', cloud_id)
+    auth_context.check_perm('cloud', 'create_resources', cloud_id)
+    auth_context.check_perm('network', 'read', network_id)
+    auth_context.check_perm('network', 'create_subnets', network_id)
+    tags = auth_context.check_perm("subnet", "add", None) or {}
+
+    try:
+        cloud = Cloud.objects.get(id=cloud_id, owner=auth_context.owner)
+    except Cloud.DoesNotExist:
+        raise CloudNotFoundError()
+    try:
+        network = Network.objects.get(id=network_id, cloud=cloud)
+    except Network.DoesNotExist:
+        raise NetworkNotFoundError()
+
+    # Create subnet.
+    subnet = SUBNETS[cloud.ctl.provider].add(network=network, **params)
+    if tags:
+        add_tags_to_resource(auth_context.owner, subnet, tags)
+
+    return subnet.as_dict()
+
+
+@view_config(route_name='api_v1_subnet', request_method='DELETE')
+def delete_subnet(request):
+    """
+    Tags: networks
+    ---
+    Delete a subnet
+
+    READ permission required on cloud
+    READ permission required on network
+    READ permission required on subnet
+    REMOVE permission required on subnet
+    ---
+    cloud_id:
+      in: path
+      required: true
+      type: string
+    network_id:
+      in: path
+      required: true
+      type: string
+    subnet_id:
+      in: path
+      required: true
+      type: string
+    """
+    cloud_id = request.matchdict['cloud']
+    subnet_id = request.matchdict['subnet']
+    network_id = request.matchdict['network']
+
+    auth_context = auth_context_from_request(request)
+
+    # SEC
+    auth_context.check_perm('cloud', 'read', cloud_id)
+    auth_context.check_perm('network', 'read', network_id)
+    auth_context.check_perm('subnet', 'read', subnet_id)
+    auth_context.check_perm('subnet', 'remove', subnet_id)
+
+    try:
+        cloud = Cloud.objects.get(id=cloud_id, owner=auth_context.owner)
+    except Cloud.DoesNotExist:
+        raise CloudNotFoundError()
+    try:
+        network = Network.objects.get(id=network_id, cloud=cloud)
+    except Network.DoesNotExist:
+        raise NetworkNotFoundError()
+    try:
+        subnet = Subnet.objects.get(id=subnet_id, network=network)
+        subnet.ctl.delete()
+    except Subnet.DoesNotExist:
+        raise SubnetNotFoundError()
+
+    # Trigger a UI update.
+    trigger_session_update(auth_context.owner, ['clouds'])
     return OK
 
 
