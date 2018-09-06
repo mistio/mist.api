mistio/mist.api

commit 0b35cf8e9ffd4b625b6aa9271f7cbb742d7407c0

Properly pass params to scheduled scripts

src/mist/api/schedules/models.py
@@ -141,10 +141,15 @@ class ActionTask(BaseTaskType):
 
 class ScriptTask(BaseTaskType):
     script_id = me.StringField()
+    params = me.StringField()
 
     @property
     def args(self):
         return self.script_id
+
+    @property
+    def kwargs(self):
+        return {'params': self.params}
 
     @property
     def task(self):
@@ -269,7 +274,7 @@ class Schedule(OwnershipMixin, me.Document, ConditionalClassMixin):
 
     @property
     def kwargs(self):
-        return {}
+        return self.task_type.kwargs
 
     @property
     def task(self):

src/mist/api/schedules/base.py
@@ -163,7 +163,8 @@ class BaseController(object):
         if action:
             self.schedule.task_type = schedules.ActionTask(action=action)
         elif script_id:
-            self.schedule.task_type = schedules.ScriptTask(script_id=script_id)
+            self.schedule.task_type = schedules.ScriptTask(
+                script_id=script_id, params=kwargs.pop('params', ''))
 
         schedule_type = kwargs.pop('schedule_type', '')
 

src/mist/api/tasks.py
@@ -1101,7 +1101,7 @@ def run_machine_action(owner_id, action, name, machine_uuid):
 
 
 @app.task
-def group_run_script(owner_id, script_id, name, machines_uuids):
+def group_run_script(owner_id, script_id, name, machines_uuids, params=''):
     """
     Accepts a list of lists in form  cloud_id,machine_id and pass them
     to run_machine_action like a group
@@ -1116,6 +1116,7 @@ def group_run_script(owner_id, script_id, name, machines_uuids):
     job_id = uuid.uuid4().hex
     for machine_uuid in machines_uuids:
             glist.append(run_script.s(owner_id, script_id, machine_uuid,
+                                      params=params,
                                       job_id=job_id, job='schedule'))
 
     schedule = Schedule.objects.get(owner=owner_id, name=name, deleted=None)

commit f854a201d57b3f1ca6284fff3f99ddeabd8ab854

Fix scheduled actions
Bug introduced in 0b35cf8e9ffd4b625b6aa9271f7cbb742d7407c0

src/mist/api/schedules/models.py
@@ -120,6 +120,10 @@ class BaseTaskType(me.EmbeddedDocument):
         raise NotImplementedError()
 
     @property
+    def kwargs(self):
+        raise NotImplementedError()
+
+    @property
     def task(self):
         raise NotImplementedError()
 
@@ -130,6 +134,10 @@ class ActionTask(BaseTaskType):
     @property
     def args(self):
         return self.action
+
+    @property
+    def kwargs(self):
+        return {}
 
     @property
     def task(self):