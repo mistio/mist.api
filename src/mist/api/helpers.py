4e524426c35e4d866767f68fd364ffd3b16f10b2
Dimitris Rozakis <dimrozakis@gmail.com>
2018-09-26 04:16:07 +0300

    Use distinct channel for checking exchange existence

--- src/mist/api/helpers.py
+++ src/mist/api/helpers.py
@@ -335,12 +335,13 @@ def amqp_owner_listening(owner):
     exchange = kombu.Exchange(_amqp_owner_exchange(owner), type='fanout')
     with kombu.pools.connections[kombu.Connection(config.BROKER_URL)].acquire(
             block=True, timeout=10) as connection:
-        try:
-            exchange(connection).declare(passive=True)
-        except AmqpNotFound:
-            return False
-        else:
-            return True
+        with connection.channel() as channel:
+            try:
+                exchange(connection.channel).declare(passive=True)
+            except AmqpNotFound:
+                return False
+            else:
+                return True
 
 
 def trigger_session_update(owner, sections=['clouds', 'keys', 'monitoring',
