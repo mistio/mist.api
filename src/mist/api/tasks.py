70ca4767fdb927b72cfaeeb8e715af6d0f545831
Dimitris Moraitis <dimo@mist.io>
2019-02-19 01:15:25 +0200

    Use the hashbang defined shell when running scripts

--- src/mist/api/tasks.py
+++ src/mist/api/tasks.py
@@ -1212,7 +1212,7 @@ def run_script(owner, script_id, machine_uuid, params='', host='',
         exit_code, wstdout = shell.command("command -v python")
 
         if exit_code > 0:
-            command = "/bin/sh %s %s" % (path, params)
+            command = "chmod +x %s && %s %s" % (path, path, params)
         else:
             command = "python - %s << EOF\n%s\nEOF\n" % (wparams, wscript)
         if su:
