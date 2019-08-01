c4d47ebaaedad3fe3c8145bc6958274ff6709724
Dimitris Moraitis <dimo@mist.io>
2019-08-01 21:24:22 +0300

    Fix indentation

--- src/mist/api/machines/views.py
+++ src/mist/api/machines/views.py
@@ -392,8 +392,8 @@ def create_machine(request):
     auth_context.check_perm("cloud", "read", cloud_id)
     auth_context.check_perm("cloud", "create_resources", cloud_id)
     if location_id:
-      auth_context.check_perm("location", "read", location_id)
-      auth_context.check_perm("location", "create_resources", location_id)
+        auth_context.check_perm("location", "read", location_id)
+        auth_context.check_perm("location", "create_resources", location_id)
     tags = auth_context.check_perm("machine", "create", None) or {}
     if script_id:
         auth_context.check_perm("script", "run", script_id)
