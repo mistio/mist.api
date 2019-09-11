df90ca3dcafaceb795e6c414a53e9d725df4bdf1
Dimitris Moraitis <dimo@mist.io>
2019-09-11 22:06:29 +0300

    Add missing imports

--- src/mist/api/models.py
+++ src/mist/api/models.py
@@ -6,6 +6,7 @@ from mist.api.keys.models import Key  # noqa
 from mist.api.scripts.models import Script  # noqa
 from mist.api.schedules.models import Schedule  # noqa
 from mist.api.users.models import User, Organization, Owner  # noqa
+from mist.api.dns.models import Zone, Record  # noqa
 
 try:
     from mist.orchestration.models import Template, Stack  # noqa
