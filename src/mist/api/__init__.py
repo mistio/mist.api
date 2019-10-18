be58a5efcb9f33f0014f1e5aaa9fde497e951d92
mpempekos <spirostza77@hotmail.com>
2019-10-18 13:31:52 +0300

    Use write concern globally

--- src/mist/api/__init__.py
+++ src/mist/api/__init__.py
@@ -32,7 +32,8 @@ def mongo_connect(*args, **kwargs):
         try:
             log.info("Attempting to connect to %s at %s...", config.MONGO_DB,
                      config.MONGO_URI)
-            me.connect(db=config.MONGO_DB, host=config.MONGO_URI)
+            #me.connect(db=config.MONGO_DB, host=config.MONGO_URI)
+            me.connect(db=config.MONGO_DB, host=config.MONGO_URI, w=1, j=True)
         except Exception as e:
             log.warning("Error connecting to mongo, will retry in 1 sec: %r",
                         e)
