a6b24843f57802144c8d8299948377be7adb23ff
Vasileios Papavasileiou <vpapav96@gmail.com>
2019-11-06 17:54:07 +0200

    refactored foundationdb handlers

--- src/mist/api/monitoring/foundationdb/handlers.py
+++ src/mist/api/monitoring/foundationdb/handlers.py
@@ -1,196 +1,214 @@
 """Set of handlers to query from Foundationdb"""
-from mist.api.exceptions import ForbiddenError
 import fdb
 import fdb.tuple
 import datetime
-fdb.api_version(610)  # set api version for fdb
+
+fdb.api_version(610)
+
+
+def create_key_tuple_second(time, machine, metric):
+    return (
+        machine,
+        metric,
+        time.year,
+        time.month,
+        time.day,
+        time.hour,
+        time.minute,
+        time.second,
+    )
+
+
+def create_key_tuple_minute(time, machine, metric):
+    return (
+        machine,
+        metric,
+        time.year,
+        time.month,
+        time.day,
+        time.hour,
+        time.minute,
+    )
+
+
+def create_key_tuple_hour(time, machine, metric):
+    return (
+        machine,
+        metric,
+        time.year,
+        time.month,
+        time.day,
+        time.hour,
+    )
+
+
+def create_timestamp_second(tuple_key):
+    # The last 6 items of the tuple contain the date up to the second
+    # (year, month, day, hour, minute, second)
+    return int(datetime.datetime(*tuple_key[-6:]).timestamp())
+
+
+def create_timestamp_minute(tuple_key):
+    # The last 5 items of the tuple contain the date up to the minute
+    # (year, month, day, hour, minute)
+    return int(datetime.datetime(*tuple_key[-5:]).timestamp())
+
+
+def create_timestamp_hour(tuple_key):
+    # The last 4 items of the tuple contain the date up to the hour
+    # (year, month, day, hour)
+    return int(datetime.datetime(*tuple_key[-4:]).timestamp())
+
+
+def create_start_stop_key_tuples(
+    time_range_in_hours, monitoring, machine, metric, start, stop
+):
+    # if time range is less than an hour, we create the keys for getting the
+    # datapoints per second
+    if time_range_in_hours <= 1:
+        return [
+            monitoring.pack(create_key_tuple_second(start, machine, metric)),
+            monitoring.pack(create_key_tuple_second(stop, machine, metric)),
+        ]
+    # in a range greater than an hour, we create the keys for getting the
+    # summarizeddatapoints per minute
+    elif 1 < time_range_in_hours <= 3:
+        return [
+            monitoring["metric_per_minute"].pack(
+                create_key_tuple_minute(start, machine, metric)
+            ),
+            monitoring["metric_per_minute"].pack(
+                create_key_tuple_minute(stop, machine, metric)
+            ),
+        ]
+    # in a range between 3 hours and 5 days, we create the keys for getting
+    # the summarized datapoints per hour
+    elif 3 < time_range_in_hours <= 120:
+        return [
+            monitoring["metric_per_hour"].pack(
+                create_key_tuple_hour(start, machine, metric)
+            ),
+            monitoring["metric_per_hour"].pack(
+                create_key_tuple_hour(stop, machine, metric)
+            ),
+        ]
+    else:
+        print("error")
+    # in a range greater than 5
+    """elif time_range_in_hours > 120:"""
+
+
+def create_timestamp(time_range_in_hours, tuple_key):
+    # if time range is less than an hour, we create the timestamp per second
+    if time_range_in_hours <= 1:
+        timestamp = create_timestamp_second(tuple_key)
+    # in a range greater than an hour, we create the timestamp per minute
+    elif 1 < time_range_in_hours <= 3:
+        timestamp = create_timestamp_minute(tuple_key)
+    # in a range between 3 hours and 5 days, we create the timestamp per hour
+    elif 3 < time_range_in_hours <= 120:
+        timestamp = create_timestamp_hour(tuple_key)
+    else:
+        print("error")
+    # in a range greater than 5
+    """elif time_range_in_hours > 120:"""
+    return timestamp
+
+
+def create_datapoint(time_range_in_hours, tuple_value, tuple_key):
+    timestamp = create_timestamp(time_range_in_hours, tuple_key)
+    # if the range is less than an hour, we create the appropriate datapoint [value, timestamp]
+    if time_range_in_hours <= 1:
+        return [tuple_value[0], timestamp]
+    # else we need to use the summarized values [sum, count, min, max]
+    # and convert them to a datapoint [value, timestamp]
+    else:
+        sum_values = tuple_value[0]
+        count = tuple_value[1]
+        return [sum_values / count, timestamp]
+
+
+def create_metric_data(metric, datapoints):
+    """name is on get load is the machine_id we will see"""
+    return {
+        metric: {
+            "id": metric,
+            "name": metric,
+            "column": metric,
+            "measurement": "system",
+            "datapoints": datapoints,
+            "max_value": None,
+            "min_value": None,
+            "priority": 0,
+            "unit": "",
+        }
+    }
 
 
 def get_data(machines, start, stop, metrics):
     db = fdb.open()
     results = {}
 
-    # get data frequency depending on time range
+    monitoring = None
+    # Open the monitoring directory if it exists
+    if fdb.directory.exists(db, "monitoring"):
+        monitoring = fdb.directory.open(db, ("monitoring",))
+    else:
+        print("The directory you are trying to read does not exist.")
+        return
+
     time_range = stop - start
     time_range_in_hours = round(time_range.total_seconds() / 3600, 2)
     print("Time range is: " + str(time_range_in_hours) + "hours.")
 
-    # if time range is less than an hour, we fetch the data per second
-    if time_range_in_hours <= 1:
-        data_freq = "s"
-    # in a range greater than an hour we fetch data per minute
-    elif time_range_in_hours > 1 and time_range_in_hours <= 3:
-        data_freq = "m"
-    # in a range between 3 hours and 5 days
-    elif time_range_in_hours > 3 and time_range_in_hours <= 120:
-        data_freq = "h"
-    # in a range greater than 5
-    elif time_range_in_hours > 120:
-        data_freq = "d"
+    print("Start time for metrics:" + str(start))
+    print("Stop time for metrics:" + str(stop))
 
-    print("data frequency is:" + data_freq)
+    for machine in machines:
+        results_machine = {}
+        for metric in metrics:
+            datapoints = []
+            (
+                key_timestamp_start,
+                key_timestamp_stop,
+            ) = create_start_stop_key_tuples(
+                time_range_in_hours, monitoring, machine, metric, start, stop
+            )
+            try:
+                #  get the range
+                for k, v in db[key_timestamp_start:key_timestamp_stop]:
 
-    # Open the monitoring directory if it exists
-    if fdb.directory.exists(db, "monitoring"):
-        monitoring = fdb.directory.open(db, ("monitoring",))
+                    tuple_key = list(fdb.tuple.unpack(k))
+                    tuple_value = list(fdb.tuple.unpack(v))
 
-        print("Start time for metrics:" + str(start))
-        print("Stop time for metrics:" + str(stop))
-
-        for machine in machines:
-            results_machine = {}
-            for metric in metrics:
-                datapoints = [[]]
-                print("Keys in this timestamp range:")
-                count = 0
-                try:
-                    if data_freq == "s":
-                        print("fetching data per second..")
-
-                        # encode keys from start/stop params
-                        tuple_key_start = (
-                            machine,
-                            metric,
-                            start.year,
-                            start.month,
-                            start.day,
-                            start.hour,
-                            start.minute,
-                            start.second,
+                    datapoints.append(
+                        create_datapoint(
+                            time_range_in_hours, tuple_value, tuple_key
                         )
+                    )
 
-                        tuple_key_stop = (
-                            machine,
-                            metric,
-                            stop.year,
-                            stop.month,
-                            stop.day,
-                            stop.hour,
-                            stop.minute,
-                            stop.second,
-                        )
-
-                        key_timestamp_start = monitoring.pack(tuple_key_start)
-                        key_timestamp_stop = monitoring.pack(tuple_key_stop)
-
-                    elif data_freq == "m":
-                        print("fetching data per minute..")
-
-                        #  open the subspace for metrics_per_minute
-                        metric_per_minute = monitoring["metric_per_minute"]
-
-                        #  encode keys from start/stop params
-                        tuple_key_start = (
-                            machine,
-                            metric,
-                            start.year,
-                            start.month,
-                            start.day,
-                            start.hour,
-                            start.minute,
-                        )
-
-                        tuple_key_stop = (
-                            machine,
-                            metric,
-                            stop.year,
-                            stop.month,
-                            stop.day,
-                            stop.hour,
-                            stop.minute,
-                        )
-
-                        key_timestamp_start = metric_per_minute.pack(
-                            tuple_key_start
-                        )
-                        key_timestamp_stop = metric_per_minute.pack(
-                            tuple_key_stop
-                        )
-
-                    elif data_freq == "h":
-                        #  open the subspace for metrics_per_minute
-                        metric_per_hour = monitoring["metric_per_hour"]
-
-                        #  encode keys from start/stop params
-                        tuple_key_start = (
-                            machine,
-                            metric,
-                            start.year,
-                            start.month,
-                            start.day,
-                            start.hour,
-                        )
-
-                        tuple_key_stop = (
-                            machine,
-                            metric,
-                            stop.year,
-                            stop.month,
-                            stop.day,
-                            stop.hour,
-                        )
-
-                        key_timestamp_start = metric_per_hour.pack(
-                            tuple_key_start
-                        )
-                        key_timestamp_stop = metric_per_hour.pack(
-                            tuple_key_stop
-                        )
-                    # TODO data_Freq for days
-
-                    print("Start time tuple key:" + str(tuple_key_start))
-                    print("Stop time tuple key:" + str(tuple_key_stop))
-
-                    #  get the range
-                    for k, v in db[key_timestamp_start:key_timestamp_stop]:
-
-                        timestamp_keys = list(fdb.tuple.unpack(k))
-
-                        #  create the timestamp from the keys list
-                        if data_freq == "s":
-                            timestamp_value = datetime.datetime(
-                                *timestamp_keys[3:9]
-                            )
-                        elif data_freq == "m":
-                            timestamp_value = datetime.datetime(
-                                *timestamp_keys[4:9]
-                            )
-                        elif data_freq == "h":
-                            timestamp_value = datetime.datetime(
-                                *timestamp_keys[4:8]
-                            )
-
-                        timestamp_value = timestamp_value.timestamp()
-                        # convert the value tuple to list
-                        tuple_list = list(fdb.tuple.unpack(v))
-                        # append the timestamp string to the list
-                        tuple_list.append(str(int(timestamp_value)))
-                        # append value list to the datapoints list
-                        datapoints.insert(count, tuple_list)
-                        count += 1
-
-                    """name is on get load is the machine_id we will see"""
-                    data = {
-                        metric: {
-                            "id": metric,
-                            "name": metric,
-                            "column": metric,
-                            "measurement": "system",
-                            "datapoints": datapoints,
-                            "max_value": None,
-                            "min_value": None,
-                            "priority": 0,
-                            "unit": "",
-                        }
-                    }
-                    results_machine.update(data)
-                except fdb.FDBError as error:
-                    db.on_error(error).wait()
-            data_machine = {machine: results_machine}
-            results.update(data_machine)
-    else:
-        print("The directory you are trying to read does not exist.")
+            except fdb.FDBError as error:
+                db.on_error(error).wait()
+            results_machine.update(create_metric_data(metric, datapoints))
+        data_machine = {machine: results_machine}
+        results.update(data_machine)
     return results
+
+
+def create_metric(data_tuple):
+    return {
+        data_tuple[1] + "." +
+        data_tuple[2]: {
+            "id": data_tuple[1] + "." + data_tuple[2],
+            "name": data_tuple[1] + "." + data_tuple[2],
+            "column": data_tuple[1] + "." + data_tuple[2],
+            "measurement": data_tuple[1] + "." + data_tuple[2],
+            "max_value": None,
+            "min_value": None,
+            "priority": 0,
+            "unit": "",
+        }
+    }
 
 
 def get_metrics(machine):
@@ -201,24 +219,10 @@ def get_metrics(machine):
 
         for k, v in db[monitoring["available_metrics"][machine].range()]:
             data_tuple = monitoring["available_metrics"][machine].unpack(k)
-            # print(data_tuple)
-            metric = {
-                data_tuple[1] +
-                "." +
-                data_tuple[2]: {
-                    "id": data_tuple[1] + "." + data_tuple[2],
-                    "name": data_tuple[1] + "." + data_tuple[2],
-                    "column": data_tuple[1] + "." + data_tuple[2],
-                    "measurement": data_tuple[1] + "." + data_tuple[2],
-                    "max_value": None,
-                    "min_value": None,
-                    "priority": 0,
-                    "unit": "",
-                }
-            }
-            metrics.update(metric)
+            metrics.update(create_metric(data_tuple))
 
     else:
-        raise ForbiddenError("Machine doesn't have the metrics directory.")
+        print("Machine doesn't have the metrics directory.")
+        return
 
     return metrics
