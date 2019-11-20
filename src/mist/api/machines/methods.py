a9de0737fb06f8d0838a879a4827ef7e4757f569
mpempekos <spirostza77@hotmail.com>
2019-11-21 01:34:42 +0200

    Do not produce observation log on first_run

--- src/mist/api/machines/methods.py
+++ src/mist/api/machines/methods.py
@@ -478,7 +478,8 @@ def create_machine(auth_context, cloud_id, key_id, machine_name, location_id,
                                 cloud_id=cloud_id)
     fresh_machines = cloud.ctl.compute._list_machines()
     cloud.ctl.compute.produce_and_publish_patch(cached_machines,
-                                                fresh_machines)
+                                                fresh_machines,
+                                                first_run=True)
 
     # Call post_deploy_steps for every provider FIXME: Refactor
     if conn.type == Provider.AZURE:
