d6ad6287d0d0008b2ee61cb28267fc386a8348af
Dimitris Moraitis <dimo@mist.io>
2019-11-28 02:23:41 +0200

    Fix logging of actions on keys

--- src/mist/api/helpers.py
+++ src/mist/api/helpers.py
@@ -943,7 +943,7 @@ def logging_view_decorator(func):
         params = dict(params_from_request(request))
         for key in ['email', 'cloud', 'machine', 'rule', 'script_id',
                     'tunnel_id', 'story_id', 'stack_id', 'template_id',
-                    'zone', 'record', 'network', 'subnet', 'volume']:
+                    'zone', 'record', 'network', 'subnet', 'volume', 'key']:
             if key != 'email' and key in request.matchdict:
                 if not key.endswith('_id'):
                     log_dict[key + '_id'] = request.matchdict[key]
