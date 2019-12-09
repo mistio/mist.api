afebfa9be4809dddca3909aacf248cb96d0ecb13
Alex Giavaras <a.giavaras@gmail.com>
2019-12-09 12:48:46 +0200

    Add LXD to SUPPORTED_PROVIDERS list

--- src/mist/api/config.py
+++ src/mist/api/config.py
@@ -1361,6 +1361,12 @@ SUPPORTED_PROVIDERS = [
         'provider': Provider.MAXIHOST,
         'regions': []
     },
+    #LXD
+    {
+      'title': 'LXD',
+       'provider':'lxd',
+        'regions':[]
+    },
 ]
 
 # Base AMIs
