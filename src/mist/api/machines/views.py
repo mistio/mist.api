eb5b7e43233f63ecc536782a0c042cefd694644d
Eis <monoatomic.alex@gmail.com>
2020-02-14 17:49:26 +0200

    Added async list_nodes and fixed create_machine issues

--- src/mist/api/machines/views.py
+++ src/mist/api/machines/views.py
@@ -1009,17 +1009,6 @@ def machine_console(request):
         ws_uri = '%s/proxy/%s/%s/%s/%s/%s/%s' % (
             base_ws_uri, host, key_id, vnc_host, vnc_port, expiry, mac)
         return render_to_response('../templates/novnc.pt', {'url': ws_uri})
-<<<<<<< HEAD
-
-    console_uri = machine.cloud.ctl.compute.connection.ex_open_console(
-            "VNC console only supported for vSphere and OpenStack")
-    url_param = machine.cloud.ctl.compute.connection.ex_open_console(
-        machine.machine_id
-    )
-    params = urllib.parse.urlencode({'url': url_param})
-    console_url = f"/ui/assets/vsphere-console-util-js/console.html?{params}"
-    raise RedirectError(console_url)
-=======
     if machine.cloud.ctl.provider == 'vsphere':
         url_param = machine.cloud.ctl.compute.connection.ex_open_console(
             machine.machine_id
@@ -1027,4 +1016,3 @@ def machine_console(request):
         params = urllib.parse.urlencode({'url': url_param})
         console_url = f"/ui/assets/vsphere-console-util-js/console.html?{params}"
         raise RedirectError(console_url)
->>>>>>> 3810f4b9... Fixed console conflict
