d2897207c02a7c0d0fb398e1b4166de70213f930
mpempekos <spirostza77@hotmail.com>
2020-03-26 18:55:22 +0200

    Make it work in case url includes the postfix

--- src/mist/api/clouds/controllers/main/controllers.py
+++ src/mist/api/clouds/controllers/main/controllers.py
@@ -113,10 +113,11 @@ class GigG8MainController(BaseMainController):
     NetworkController = network_ctls.GigG8NetworkController
 
     def _add__preparse_kwargs(self, kwargs):
-        base_url = kwargs.pop('url')
-        if base_url.endswith('/'):
-            base_url = base_url[:-1]
-        kwargs['url'] = base_url + '/restmachine/cloudapi/'
+        if not kwargs['url'].endswith('/restmachine/cloudapi'):
+            base_url = kwargs.pop('url')
+            if base_url.endswith('/'):
+                base_url = base_url[:-1]
+            kwargs['url'] = base_url + '/restmachine/cloudapi/'
 
 
 class LinodeMainController(BaseMainController):
