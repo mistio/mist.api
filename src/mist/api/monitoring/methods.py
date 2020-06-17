4d28dd8883fb499c5d8d95aa15ef1a84272485fb
Vasileios Papavasileiou <vpapav96@gmail.com>
2020-06-17 16:48:01 +0300

    Add get_cores monitoring method

--- src/mist/api/monitoring/methods.py
+++ src/mist/api/monitoring/methods.py
@@ -29,6 +29,8 @@ from mist.api.monitoring.influxdb.handlers \
     import MainStatsHandler as InfluxMainStatsHandler
 from mist.api.monitoring.influxdb.handlers \
     import MultiLoadHandler as InfluxMultiLoadHandler
+from mist.api.monitoring.influxdb.handlers \
+    import MultiCoresHandler as InfluxMultiCoresHandler
 
 from mist.api.monitoring.graphite.methods \
     import get_stats as graphite_get_stats
@@ -36,6 +38,8 @@ from mist.api.monitoring.graphite.methods \
     import find_metrics as graphite_find_metrics
 from mist.api.monitoring.graphite.methods \
     import get_load as graphite_get_load
+from mist.api.monitoring.graphite.methods \
+    import get_cores as graphite_get_cores
 
 from mist.api.monitoring import traefik
 
@@ -132,8 +136,11 @@ def get_load(owner, start='', stop='', step='', uuids=None):
         # Transform "min" and "sec" to "m" and "s", respectively.
         _start, _stop, _step = [re.sub('in|ec', repl='', string=x) for x in (
             start.strip('-'), stop.strip('-'), step)]
+        metric = "system.load1"
+        if step:
+            metric = "MEAN(%s)" % metric
         influx_data = InfluxMultiLoadHandler(influx_uuids).get_stats(
-            metric='system.load1',
+            metric=metric,
             start=_start, stop=_stop, step=_step,
         )
 
@@ -141,6 +148,56 @@ def get_load(owner, start='', stop='', step='', uuids=None):
         return dict(list(graphite_data.items()) + list(influx_data.items()))
     else:
         raise NotFoundError('No machine has monitoring enabled')
+
+
+def get_cores(owner, start="", stop="", step="", uuids=None):
+    """Get cores for all monitored machines."""
+    clouds = Cloud.objects(owner=owner, deleted=None).only("id")
+    machines = Machine.objects(
+        cloud__in=clouds, monitoring__hasmonitoring=True
+    )
+    if uuids:
+        machines.filter(id__in=uuids)
+
+    graphite_uuids = [
+        machine.id
+        for machine in machines
+        if machine.monitoring.method.endswith("-graphite")
+    ]
+    influx_uuids = [
+        machine.id
+        for machine in machines
+        if machine.monitoring.method.endswith("-influxdb")
+    ]
+
+    graphite_data = {}
+    influx_data = {}
+
+    if graphite_uuids:
+        graphite_data = graphite_get_cores(
+            owner, start=start, stop=stop, step=step, uuids=graphite_uuids
+        )
+    if influx_uuids:
+        # Transform "min" and "sec" to "m" and "s", respectively.
+        _start, _stop, _step = [
+            re.sub("in|ec", repl="", string=x)
+            for x in (start.strip("-"), stop.strip("-"), step)
+        ]
+        metric = "cpu.cpu=/cpu\d/.usage_idle"
+        if step:
+            metric = "MEAN(%s)" % metric
+        influx_data = InfluxMultiCoresHandler(influx_uuids).get_stats(
+            metric=metric, start=_start, stop=_stop,
+            step=_step,
+        )
+
+    if graphite_data or influx_data:
+        return dict(
+            list(graphite_data.items()) +
+            list(influx_data.items())
+        )
+    else:
+        raise NotFoundError("No machine has monitoring enabled")
 
 
 def check_monitoring(owner):
