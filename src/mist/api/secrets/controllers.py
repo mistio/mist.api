d21f2d9caa407295e538a600c15de0f1ef734970
luserx0 <luserx0@gmail.com>
2020-08-28 20:12:18 +0300

    Create {list,delete,update}-secret functions

--- src/mist/api/secrets/controllers.py
+++ src/mist/api/secrets/controllers.py
@@ -26,16 +26,41 @@ class VaultSecretController(BaseSecretController):
         self.client = hvac.Client(url=url, token=token)
         assert(self.client.is_authenticated())
 
+    def secret_type(self):
+        """ Get Secret's type """
+
+        what_secret = self.client.list_secret_backends()\
+            [self.secret.secret_engine_name + '/']['type']
+
+        return what_secret
+
     def list_secrets(self):
         """ List all available Secrets in Secret Engine """
-        print(self.client.list(self.secret.secret_engine_name)['data'])
+
+         # Read version and map the create_secret
+        version = self.secret_type()
+
+        if version == 'kv':
+            list_secrets = self.client.secrets.kv.v1.list_secrets
+        elif version == 'kv2':
+            list_secrets = self.client.secrets.kv.v2.list_secrets
+
+        api_secrets_result = list_secrets(
+            mount_point = self.secret.secret_engine_name,
+            path = self.secret.name
+        )
+
+        print('The following keys found under the selected path ("/v1/secret/{path}"): {keys}'.format(
+            path = self.secret.secret_engine_name,
+            keys = ','.join(api_secrets_result['data']['keys']),
+        ))
 
     def list_secret_engines(self):
         """ List all available Secret Engines """
         print(self.client.list_secret_backends())
 
     def create_secret(self, data):
-        """ Create a Vault Secret """
+        """ Create a Vault KV* Secret """
 
         # Read version and map the create_secret
         version = self.secret_type()
@@ -55,16 +80,8 @@ class VaultSecretController(BaseSecretController):
                 path=self.secret.name
         ))
 
-    def secret_type(self):
-        """ Get Secret's type """
-
-        what_secret = self.client.list_secret_backends()\
-            [self.secret.secret_engine_name + '/']['type']
-
-        return what_secret
-
     def read_secret(self):
-        """ Read a Vault KV Secret """
+        """ Read a Vault KV* Secret """
 
         # Read version and map the read_secret
         version = self.secret_type()
@@ -79,3 +96,21 @@ class VaultSecretController(BaseSecretController):
             path=self.secret.name
         )
         return api_response
+
+    def delete_secret(self):
+        " Delete a Vault KV* Secret"
+
+        # Read version and map the read_secret
+        version = self.secret_type()
+
+        if version == 'kv':
+            delete_secret = self.client.secrets.kv.v1.delete_secret
+        elif version == 'kv2':
+            delete_secret = self.client.secrets.kv.v2.delete_secret
+
+        delete_secret(
+            mount_point=self.secret.secret_engine_name,
+            path=self.secret.name
+        )
+
+    def update_secret(self): self.create_secret
