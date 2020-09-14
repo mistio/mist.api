2047a47aa40fb32eafc4fa98e0128a676ee68758
mpempekos <spirostza77@hotmail.com>
2020-09-14 16:48:20 +0300

    Fix checking provider if key_id not provided. Update libcloud

--- src/mist/api/machines/methods.py
+++ src/mist/api/machines/methods.py
@@ -297,11 +297,11 @@ def create_machine(auth_context, cloud_id, key_id, machine_name, location_id,
     # if key_id not provided, search for default key
     if cloud.ctl.provider not in [Provider.LIBVIRT.value,
                                   Container_Provider.DOCKER,
-                                  Provider.ONAPP,
-                                  Provider.AZURE_ARM,
-                                  Provider.GIG_G8,
-                                  Provider.VSPHERE,
-                                  Provider.KUBEVIRT,
+                                  Provider.ONAPP.value,
+                                  Provider.AZURE_ARM.value,
+                                  Provider.GIG_G8.value,
+                                  Provider.VSPHERE.value,
+                                  Provider.KUBEVIRT.value,
                                   Container_Provider.LXD]:
         if not key_id:
             try:
