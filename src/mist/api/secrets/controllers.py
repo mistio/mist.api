d7bdab72046bc879fbfcbf25a424fc51e2be881b
mpempekos <spirostza77@hotmail.com>
2020-10-21 22:15:11 +0300

    Flake8 fixes

--- src/mist/api/secrets/controllers.py
+++ src/mist/api/secrets/controllers.py
@@ -5,6 +5,7 @@ from mist.api import config
 
 log = logging.getLogger(__name__)
 
+
 class BaseSecretController(object):
     def __init__(self, secret):
         """Initialize a secret controller given a secret
@@ -40,9 +41,11 @@ class VaultSecretController(BaseSecretController):
             )
         except hvac.exceptions.InvalidPath:
             # create relative handler for specific org
-            log.info('No KV secret engine found for org %s. Creating one...' % org_name)
-            self.client.sys.enable_secrets_engine(backend_type='kv', path=org_name,
-                                                  options={'version':2}
+            log.info('No KV secret engine found for org %s. \
+                Creating one...' % org_name)
+            self.client.sys.enable_secrets_engine(backend_type='kv',
+                                                  path=org_name,
+                                                  options={'version': 2}
                                                   )
             self.client.secrets.kv.v2.create_or_update_secret(
                 mount_point=org_name,
@@ -53,9 +56,9 @@ class VaultSecretController(BaseSecretController):
     def read_secret(self, org_name):
         """ Read a Vault KV* Secret """
         api_response = self.client.secrets.kv.v2.read_secret_version(
-                                mount_point=org_name,
-                                path=self.secret.name
-                            )
+            mount_point=org_name,
+            path=self.secret.name
+        )
 
         return api_response
 
