3653a6f5cf3cf8a03f06895443c89c46294c2d14
mpempekos <spirostza77@hotmail.com>
2020-10-24 01:54:38 +0300

    flake8 fixes

--- src/mist/api/clouds/controllers/compute/controllers.py
+++ src/mist/api/clouds/controllers/compute/controllers.py
@@ -2615,7 +2615,8 @@ class HostVirtualComputeController(BaseComputeController):
 class EquinixMetalComputeController(BaseComputeController):
 
     def _connect(self, **kwargs):
-        project_id = self.cloud.project_id.value if self.cloud.project_id else ''
+        project_id = self.cloud.project_id.value if self.cloud.project_id \
+            else ''
         return get_driver(
             Provider.EQUINIXMETAL)(self.cloud.apikey.value,
                                    project=project_id)
@@ -3497,7 +3498,8 @@ class DockerComputeController(BaseComputeController):
         self._dockerhost = None
 
     def _connect(self, **kwargs):
-        host, port = dnat(self.cloud.owner, self.cloud.host.value, self.cloud.port.value)
+        host, port = dnat(self.cloud.owner, self.cloud.host.value,
+                          self.cloud.port.value)
 
         try:
             socket.setdefaulttimeout(15)
@@ -3544,7 +3546,8 @@ class DockerComputeController(BaseComputeController):
 
     def _list_machines__fetch_machines(self):
         """Perform the actual libcloud call to get list of containers"""
-        containers = self.connection.list_containers(all=self.cloud.show_all.value)
+        containers = self.connection.list_containers(all=self.cloud.show_all.
+                                                     value)
         # add public/private ips for mist
         for container in containers:
             public_ips, private_ips = [], []
@@ -3907,7 +3910,8 @@ class LXDComputeController(BaseComputeController):
         )
 
     def _connect(self, **kwargs):
-        host, port = dnat(self.cloud.owner, self.cloud.host.value, self.cloud.port.value)
+        host, port = dnat(self.cloud.owner, self.cloud.host.value,
+                          self.cloud.port.value)
 
         try:
             socket.setdefaulttimeout(15)
