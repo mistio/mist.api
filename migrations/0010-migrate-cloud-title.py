9c5b6880d588b8957a7e23fd117fc69c7785a112
mpempekos <spirostza77@hotmail.com>
2020-11-01 21:42:42 +0200

    Add migration script

---
+++ migrations/0010-migrate-cloud-title.py
@@ -0,0 +1,42 @@
+import traceback
+
+from pymongo import MongoClient
+from mist.api.config import MONGO_URI
+
+
+def migrate_clouds():
+    c = MongoClient(MONGO_URI)
+    db = c.get_database('mist2')
+    db_clouds = db['clouds']
+
+    failed = migrated = skipped = 0
+
+    for cloud in db_clouds.find():
+        print('Updating cloud ' + cloud['_id'])
+        try:
+            cloud_name = cloud['title']
+            db_clouds.update_one(
+                {'_id': cloud['_id']},
+                {'$unset': {'title': ''}}
+            )
+            db_clouds.update_one(
+                {'_id': cloud['_id']},
+                {'$set': {'name': cloud_name}}
+            )
+        except Exception:
+            traceback.print_exc()
+            failed += 1
+            continue
+        else:
+            print('OK')
+            migrated += 1
+
+    print('Clouds migrated: %d' % migrated)
+    if skipped:
+        print('Skipped: %d' % skipped)
+
+    c.close()
+
+
+if __name__ == '__main__':
+    migrate_clouds()
