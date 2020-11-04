1a6feb12b3c77e3aeabe3151c40eaaaa2dc77a33
mpempekos <spirostza77@hotmail.com>
2020-11-04 19:40:51 +0200

    No need to pass org_name as argument of methods inside secret.ctl

--- src/mist/api/keys/methods.py
+++ src/mist/api/keys/methods.py
@@ -45,7 +45,7 @@ def delete_key(owner, key_id, delete_from_vault=False):
     log.info("Deleted key with id '%s'.", key_id)
 
     if delete_from_vault:
-        key.private.secret.ctl.delete_secret(owner.name)
+        key.private.secret.ctl.delete_secret()
         secret = VaultSecret.objects.get(owner=owner, name=key.name)
         secret.delete()
 
