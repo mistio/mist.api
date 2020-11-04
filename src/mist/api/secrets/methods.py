efae0846394365743dd6d944c57b167cde221608
mpempekos <spirostza77@hotmail.com>
2020-11-04 21:13:54 +0200

    Cleanup code. No need to define `existing_secret`

--- src/mist/api/secrets/methods.py
+++ src/mist/api/secrets/methods.py
@@ -8,15 +8,15 @@ def value_refers_to_secret(value, owner):
         This method parses a value given, which might
         refer to a private key or to part of cloud
         credentials (eg token, api_key, certificate etc)
-        Returns (secret, key) if the value is of the following format:
-        <secret_id>:key, otherwise None, ''
+        Returns (secret, key, True) if the value is of the following format:
+        <secret_id>:key, otherwise (None, '', False)
     '''
     if len(value.split(':')) == 2:
         secret_id, key = value.split(':')
         try:
             secret = VaultSecret.objects.get(id=secret_id, owner=owner)
-            return (secret, key)
+            return (secret, key, True)
         except me.DoesNotExist:
-            return (None, '')
+            return (None, '', False)
 
-    return (None, '')
+    return (None, '', False)
