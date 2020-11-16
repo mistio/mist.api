ae43fdafa58e7bd9c2f599c41c2bfbeb2f28bf3f
dimgal1 <dimgal20@gmail.com>
2020-11-16 12:13:12 +0200

    Associate key with user and add machine tags

--- src/mist/api/dramatiq_tasks.py
+++ src/mist/api/dramatiq_tasks.py
@@ -7,6 +7,8 @@ import dramatiq
 from mist.api.clouds.models import Cloud
 from mist.api.machines.models import Machine
 from mist.api.auth.methods import AuthContext
+from mist.api.logs.methods import log_event
+from mist.api.tag.methods import resolve_id_and_set_tags
 # from mist.api.exceptions import MachineCreationError
 from mist.api.dramatiq_app import broker
 
@@ -14,7 +16,8 @@ log = logging.getLogger(__name__)
 
 
 @dramatiq.actor(queue_name='dramatiq_create_machine', broker=broker)
-def dramatiq_create_machine_async(auth_context_serialized, plan):
+def dramatiq_create_machine_async(auth_context_serialized, job_id, plan):
+
     auth_context = AuthContext.deserialize(auth_context_serialized)
     cloud = Cloud.objects.get(id=plan['cloud'])
 
@@ -36,3 +39,13 @@ def dramatiq_create_machine_async(auth_context_serialized, plan):
                     continue
 
     machine.assign_to(auth_context.user)
+    # Associate key.
+    if plan['key'] is not None:
+        from mist.api.keys.models import Key
+        key = Key.objects.get(id=plan['key'])
+        username = node.extra.get('username', '')
+        machine.ctl.associate_key(key, username=username,
+                                  port=22, no_connect=True)
+    if plan['tags']:
+        resolve_id_and_set_tags(auth_context. owner, 'machine',
+                                node.id, plan['tags'], cloud_id=cloud.id)
