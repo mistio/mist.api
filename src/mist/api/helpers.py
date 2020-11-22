05ec8b23ac02125950dda90a97c106bc171a87dc
mpempekos <spirostza77@hotmail.com>
2020-11-22 18:46:29 +0200

    Produce logs for secrets

--- src/mist/api/helpers.py
+++ src/mist/api/helpers.py
@@ -742,6 +742,7 @@ rtype_to_classpath = {
     'volume': 'mist.api.volumes.models.Volume',
     'location': 'mist.api.clouds.models.CloudLocation',
     'image': 'mist.api.images.models.CloudImage',
+    'secret': 'mist.api.secrets.models.VaultSecret',
 }
 
 if config.HAS_VPN:
@@ -973,7 +974,7 @@ def logging_view_decorator(func):
         params = dict(params_from_request(request))
         for key in ['email', 'cloud', 'machine', 'rule', 'script_id',
                     'tunnel_id', 'story_id', 'stack_id', 'template_id',
-                    'zone', 'record', 'network', 'subnet', 'volume', 'key']:
+                    'zone', 'record', 'network', 'subnet', 'volume', 'key', 'secret']:
             if key != 'email' and key in request.matchdict:
                 if not key.endswith('_id'):
                     log_dict[key + '_id'] = request.matchdict[key]
@@ -1050,7 +1051,7 @@ def logging_view_decorator(func):
                 log_dict['machine_id'] = bdict['machine']
             # Match resource type based on the action performed.
             for rtype in ['cloud', 'machine', 'key', 'script', 'tunnel',
-                          'stack', 'template', 'schedule', 'volume']:
+                          'stack', 'template', 'schedule', 'volume', 'secret']:
                 if rtype in log_dict['action']:
                     if 'id' in bdict and '%s_id' % rtype not in log_dict:
                         log_dict['%s_id' % rtype] = bdict['id']
