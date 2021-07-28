e957c9b8f4b85022bba0dbe893fac18065990889
Eis <monoatomic.alex@gmail.com>
2021-07-28 21:07:51 +0300

    Add db_type key to each victoria metrics' metric dictionary with victoria_metrics as value

--- src/mist/api/monitoring/victoriametrics/methods.py
+++ src/mist/api/monitoring/victoriametrics/methods.py
@@ -115,7 +115,12 @@ def find_metrics(machine):
     metrics = {}
     for raw_metric in data:
         metric = generate_metric_mist(raw_metric)
-        metrics.update({metric: {"id": metric, "name": metric, "unit": ""}})
+        metrics.update({metric: {
+            "id": metric,
+            "name": metric,
+            "unit": "",
+            "db_type": 'victoria_metrics'
+            }})
 
     return metrics
 
