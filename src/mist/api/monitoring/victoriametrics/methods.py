63e52d0f3401f496d87e94dc039e1aa1dbb075c5
Eis <monoatomic.alex@gmail.com>
2021-07-29 10:20:46 +0300

    Keep names consistent

--- src/mist/api/monitoring/victoriametrics/methods.py
+++ src/mist/api/monitoring/victoriametrics/methods.py
@@ -119,7 +119,7 @@ def find_metrics(machine):
             "id": metric,
             "name": metric,
             "unit": "",
-            "db_type": 'victoria_metrics'}})
+            "method": 'telegraf-victoriametrics'}})
 
     return metrics
 
