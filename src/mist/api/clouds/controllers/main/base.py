b4dd571f5e2180f2e13f2d1151604b4b8f120da2
Dimitris Moraitis <dimo@mist.io>
2021-08-08 19:00:11 +0300

    Save secret when adding cloud

--- src/mist/api/clouds/controllers/main/base.py
+++ src/mist/api/clouds/controllers/main/base.py
@@ -336,18 +336,16 @@ class BaseMainController(object):
                                              owner=self.cloud.owner)
                         # first store key in Vault
                         secret.ctl.create_or_update_secret({key: value})
-                        # FIXME this fails because create_or_update_secret
-                        # saves the secret already.
-                        # try:
-                        #     secret.save()
-                        #     if user:
-                        #         secret.assign_to(user)
-                        # except me.NotUniqueError:
-                        #     raise BadRequestError("
-                        # The path `%s%s` exists on Vault. \
-                        #         Try changing the name of the cloud" %
-                        #                           (config.VAULT_CLOUDS_PATH,
-                        #                            self.cloud.name))
+                        try:
+                            secret.save()
+                            if user:
+                                secret.assign_to(user)
+                        except me.NotUniqueError:
+                            raise BadRequestError(
+                                "The path `%s%s` exists on Vault. \
+                                 Try changing the name of the cloud" % (
+                                    config.VAULT_CLOUDS_PATH, self.cloud.name)
+                            )
 
                     try:
                         secret.ctl.create_or_update_secret({key: value})
