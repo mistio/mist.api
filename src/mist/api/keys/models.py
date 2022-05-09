610fc34a9998361a236806950a3af6b7a264f584
Eis <monoatomic.alex@gmail.com>
2022-05-09 14:23:16 +0300

    Fix bug in as_dict_v2 for key tags.

--- src/mist/api/keys/models.py
+++ src/mist/api/keys/models.py
@@ -174,7 +174,7 @@ class Key(OwnershipMixin, me.Document):
                 for tag in Tag.objects(
                     owner=self.owner,
                     resource_id=self.id,
-                    resource_type='cloud').only('key', 'value')
+                    resource_type='Key').only('key', 'value')
             }
         return ret
 
