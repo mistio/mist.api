72407d0544dcf8762ab09e1a1f578bb77a258bda
Eis <monoatomic.alex@gmail.com>
2022-05-17 10:40:09 +0300

    Add 4.7 changes to cluster and machine Cost

--- src/mist/api/clouds/controllers/container/base.py
+++ src/mist/api/clouds/controllers/container/base.py
@@ -489,15 +489,14 @@ class BaseContainerController(BaseController):
                 self._list_clusters__cost_nodes(cluster, libcloud_cluster)
             cph = nodes_cph + control_plane_cph
             cpm = nodes_cpm + control_plane_cpm
-            if(cluster.cost.hourly != cph or
-               cluster.cost.monthly != cpm or
-               cluster.cost.control_plane_hourly != control_plane_cph or
-               cluster.cost.control_plane_monthly != control_plane_cpm):
-                cluster.cost.hourly = round(cph, 2)
-                cluster.cost.monthly = round(cpm, 2)
-                cluster.cost.control_plane_hourly = round(control_plane_cph, 2)
-                cluster.cost.control_plane_monthly = round(
-                    control_plane_cpm, 2)
+            if(cluster.total_cost.hourly != round(cph, 2) or
+               cluster.total_cost.monthly != round(cpm, 2) or
+               cluster.cost.hourly != round(control_plane_cph, 2) or
+               cluster.cost.monthly != round(control_plane_cpm, 2)):
+                cluster.total_cost.hourly = round(cph, 2)
+                cluster.total_cost.monthly = round(cpm, 2)
+                cluster.cost.hourly = round(control_plane_cph, 2)
+                cluster.cost.monthly = round(
                 updated = True
         except Exception as exc:
             log.exception("Error while calculating cost "
