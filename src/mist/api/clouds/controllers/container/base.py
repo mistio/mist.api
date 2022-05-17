4fcef2fb2a262f5dcb6de64c0bd7f4b20e3579ca
Eis <monoatomic.alex@gmail.com>
2022-05-17 10:37:10 +0300

    Fix typo

--- src/mist/api/clouds/controllers/container/base.py
+++ src/mist/api/clouds/controllers/container/base.py
@@ -487,10 +487,13 @@ class BaseContainerController(BaseController):
             nodes_cph, nodes_cpm = \
                 self._list_clusters__cost_nodes(cluster, libcloud_cluster)
             cph = nodes_cph + control_plane_cph
-            if(cluster.total_cost_hourly != round(cph, 2) or
+            cpm = nodes_cpm + control_plane_cpm
+            if(cluster.total_cost.hourly != round(cph, 2) or
+               cluster.total_cost.monthly != round(cpm, 2) or
                cluster.cost.hourly != round(control_plane_cph, 2) or
                cluster.cost.monthly != round(control_plane_cpm, 2)):
-                cluster.total_cost_hourly = round(cph, 2)
+                cluster.total_cost.hourly = round(cph, 2)
+                cluster.total_cost.monthly = round(cpm, 2)
                 cluster.cost.hourly = round(control_plane_cph, 2)
                 cluster.cost.monthly = round(
                     control_plane_cpm, 2)
