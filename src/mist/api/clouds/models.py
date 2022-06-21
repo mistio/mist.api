445129022e084a1429a5e7322e3401be9b51f8a8
Dimitris Galanis <dimgal20@gmail.com>
2022-06-21 11:57:39 +0300

    Add query param on list locations endpoint to optionally return extra field

--- src/mist/api/clouds/models.py
+++ src/mist/api/clouds/models.py
@@ -400,10 +400,10 @@ class CloudLocation(OwnershipMixin, me.Document):
             ret['created'] = str(ret['created'])
         return ret
 
-    def as_dict(self):
+    def as_dict(self, extra=True):
         location_dict = {
             'id': self.id,
-            'extra': self.extra,
+            'extra': self.extra if extra else {},
             'cloud': self.cloud.id if self.cloud else None,  # same as above
             'external_id': self.external_id,
             'name': self.name,
