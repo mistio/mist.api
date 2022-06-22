1e27b6205a2432953b07c5cd7eea6ccc89fd12f7
George Alexakis <galexakhs97@gmail.com>
2022-06-22 18:19:53 +0300

    Rename docker-exec->docker-attach

--- src/mist/api/machines/methods.py
+++ src/mist/api/machines/methods.py
@@ -2742,7 +2742,7 @@ def prepare_docker_attach_uri(machine):
         msg=msg.encode(),
         digestmod=hashlib.sha256).hexdigest()
     base_ws_uri = config.CORE_URI.replace('http', 'ws')
-    exec_uri = '%s/docker-exec/%s/%s/%s/%s/%s/%s/%s/%s' % (
+    exec_uri = '%s/docker-attach/%s/%s/%s/%s/%s/%s/%s/%s' % (
         base_ws_uri,
         name,
         cluster,
