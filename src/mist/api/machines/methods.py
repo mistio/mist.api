811610cd58136b903ce3d099620033ff290842eb
George Alexakis <galexakhs97@gmail.com>
2022-06-24 19:15:03 +0300

    Use SIGN_KEY for hmac

--- src/mist/api/machines/methods.py
+++ src/mist/api/machines/methods.py
@@ -2738,7 +2738,7 @@ def prepare_docker_attach_uri(machine):
         expiry,
         encrypted_msg)
     mac = hmac.new(
-        config.SECRET.encode(),
+        config.SIGN_KEY.encode(),
         msg=msg.encode(),
         digestmod=hashlib.sha256).hexdigest()
     base_ws_uri = config.CORE_URI.replace('http', 'ws')
