d388e97f849951ff05090923b9002c7587687a3e
gkaps <gkaps@mist.io>
2022-06-27 16:03:54 +0300

    Move extract_selector_type method to helpers.py

--- src/mist/api/helpers.py
+++ src/mist/api/helpers.py
@@ -2071,3 +2071,16 @@ class WebSocketApp(object):
         self.retval = 0
         output = self.buffer.split("\n")[0:-1]
         return self.retval, "\n".join(output)
+
+
+def extract_selector_type(**kwargs):
+    error_count = 0
+    for selector in kwargs.get('selectors', []):
+        if selector['type'] not in rtype_to_classpath:
+            error_count += 1
+        if selector['ids'] is not None:
+            selector_type = selector['type'].rstrip('s')
+            break
+    if error_count == len(kwargs.get('selectors', [])):
+        raise BadRequestError('selector_type')
+    return selector_type
