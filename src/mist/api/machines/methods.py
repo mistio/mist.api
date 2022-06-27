01235c6f4cd9adaa3b8da4f088a9335c7937aff2
George Alexakis <galexakhs97@gmail.com>
2022-06-27 12:35:56 +0000

    Add kubernetes case

--- src/mist/api/machines/methods.py
+++ src/mist/api/machines/methods.py
@@ -2753,3 +2753,40 @@ def prepare_docker_attach_uri(machine):
         encrypted_msg,
         mac)
     return attach_uri
+
+def prepare_kubernetes_uri(auth_context, machine):
+    expiry = int(datetime.now().timestamp()) + 100
+    pod = machine.name
+    cluster = machine.cloud.name
+    key_path = machine.cloud.key_file.secret.name
+    org = machine.owner
+    vault_token = org.vault_token if org.vault_token is not None else \
+        config.VAULT_TOKEN
+    vault_addr = org.vault_address if org.vault_address is not None else \
+        config.VAULT_ADDR
+    vault_secret_engine_path = machine.owner.vault_secret_engine_path
+    msg_to_encrypt = '%s,%s,%s,%s' % (
+        vault_token,
+        vault_addr,
+        vault_secret_engine_path,
+        key_path)
+    # ENCRYPTION KEY AND HMAC KEY SHOULD BE DIFFERENT!
+    encrypted_msg = encrypt(msg_to_encrypt, segment_size=128)
+    msg = '%s,%s,%s,%s,%s' % (
+        pod,
+        cluster,
+        expiry,
+        encrypted_msg)
+    mac = hmac.new(
+        config.SIGN_KEY.encode(),
+        msg=msg.encode(),
+        digestmod=hashlib.sha256).hexdigest()
+    base_ws_uri = config.CORE_URI.replace('http', 'ws')
+    exec_uri = '%s/k8s-exec/%s/%s/%s/%s/%s' % (
+        base_ws_uri,
+        pod,
+        cluster,
+        expiry,
+        encrypted_msg,
+        mac)
+    return exec_uri
