baebbb07a833ee43ac511041dce422367f856536
Dimitris Galanis <dimgal20@gmail.com>
2022-07-07 20:07:36 +0300

    Add dramatiq task to check last runs of cloud polling schedules

--- src/mist/api/portal/methods.py
+++ src/mist/api/portal/methods.py
@@ -1,3 +1,4 @@
+import datetime
 import requests
 import dns.resolver
 
@@ -36,3 +37,57 @@ def active_websocket_sessions():
                     users[entry['user']]['last_rcv'] = entry['last_rcv']
 
     return users
+
+
+def should_task_exist_for_cloud(task, cloud):
+    """
+    Return whether a given cloud should have the specified scheduled task.
+    """
+    if ((task == "list_zones" and cloud.dns_enabled is False) or
+        (task == "list_buckets" and cloud.object_storage_enabled is False) or
+                (task == "list_clusters" and cloud.container_enabled is False) or  # noqa: E501
+            (task == "list_networks" and getattr(cloud.ctl, "network", None) is None) or  # noqa: E501
+            (task == "list_volumes" and getattr(cloud.ctl, "storage", None) is None) or  # noqa: E501
+        (task == "list_sizes" and cloud._cls == "Cloud.LibvirtCloud") or
+        (task != "list machines" and cloud._cls == "Cloud.OtherCloud")
+        ):
+        return False
+    return True
+
+
+def check_task_threshold(cloud, task, acceptable_timedelta):
+    """
+    Check whether the specific task for the provided cloud was scheduled
+    within the acceptable timelimit, irregardless of the tasks success or
+    failure.
+
+    If the task was not scheduled within the timedelta an error message
+    is returned indicating when the task was last run.
+    """
+    from mist.api.concurrency.models import PeriodicTaskInfo
+
+    if should_task_exist_for_cloud(task, cloud) is False:
+        return None
+
+    FMT = f"cloud:{task}:{cloud.id}"
+    try:
+        task_info = PeriodicTaskInfo.objects.get(
+            key=FMT.format(task=task, cloud_id=cloud.id))
+    except PeriodicTaskInfo.DoesNotExist:
+        return f"Periodic task {task} does not exist for cloud {cloud}"
+
+    last_success_td = datetime.datetime.now(
+    ) - task_info.last_success if task_info.last_success else None
+
+    last_failure_td = datetime.datetime.now(
+    ) - task_info.last_failure if task_info.last_failure else None
+
+    if last_success_td is None and last_failure_td is None:
+        return f"Periodic task {task} for cloud {cloud} has never run"
+    elif last_success_td and last_failure_td:
+        if min(last_success_td, last_failure_td) > acceptable_timedelta:
+            return f"Periodic task {task} was last run before {min(last_success_td, last_failure_td)} for cloud {cloud}"  # noqa: E501
+    else:
+        last_run = last_success_td or last_failure_td
+        if last_run > acceptable_timedelta:
+            return f"Periodic task {task} was last run before {last_run} for cloud {cloud}"  # noqa: E501
