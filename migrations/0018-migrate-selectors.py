20861db4973a5902678a0c2380f21bea574f17f2
Eis <monoatomic.alex@gmail.com>
2022-07-28 17:03:35 +0300

    Separate migration 0018 into two, add migration for kvm hosts to acquire ssh-probe schedule.

--- migrations/0018-migrate-selectors.py
+++ migrations/0018-migrate-selectors.py
@@ -1,5 +1,4 @@
 import traceback
-from datetime import datetime
 
 from pymongo import MongoClient
 
@@ -32,40 +31,5 @@ def migrate_selectors():
                       f'{cls} -> {newcls} - OK!')
 
 
-def cleanup_keys():
-    """
-    Removes trailing chars from private keys
-    It has been reported by users but shouldn't normally happen
-    """
-    from mist.api.keys.models import SSHKey
-    for key in SSHKey.objects(deleted=None):
-        if key.private.endswith('-'):
-            continue
-        while len(key.private) and not key.private.endswith('-'):
-            key.private = key.private[:-1]
-        key.save()
-
-
-def cleanup_libvirt_cloud_locations():
-    """
-    Marks CloudLocation objects missing if the respective host is missing
-    """
-    from mist.api.models import Machine
-    from mist.api.clouds.models import CloudLocation, LibvirtCloud
-    libvirt_cloud_ids = [l.id for l in LibvirtCloud.objects(
-        deleted=None).only('id')]
-
-    for loc in CloudLocation.objects(cloud__in=libvirt_cloud_ids):
-        try:
-            Machine.objects.get(
-                cloud=loc.cloud, missing_since=None,
-                machine_id=loc.external_id)
-        except Machine.DoesNotExist:
-            loc.missing_since = datetime.now()
-            loc.save()
-
-
 if __name__ == '__main__':
     migrate_selectors()
-    cleanup_keys()
-    cleanup_libvirt_cloud_locations()
---
+++ migrations/0019-cleanup-keys-libvirt-locations.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+
+def cleanup_keys():
+    """
+    Removes trailing chars from private keys
+    It has been reported by users but shouldn't normally happen
+    """
+    from mist.api.keys.models import SSHKey
+    for key in SSHKey.objects(deleted=None):
+        if key.private.endswith('-'):
+            continue
+        while len(key.private) and not key.private.endswith('-'):
+            key.private = key.private[:-1]
+        key.save()
+
+
+def cleanup_libvirt_cloud_locations():
+    """
+    Marks CloudLocation objects missing if the respective host is missing
+    """
+    from mist.api.models import Machine
+    from mist.api.clouds.models import CloudLocation, LibvirtCloud
+    libvirt_cloud_ids = [l.id for l in LibvirtCloud.objects(
+        deleted=None).only('id')]
+
+    for loc in CloudLocation.objects(cloud__in=libvirt_cloud_ids):
+        try:
+            Machine.objects.get(
+                cloud=loc.cloud, missing_since=None,
+                machine_id=loc.external_id)
+        except Machine.DoesNotExist:
+            loc.missing_since = datetime.now()
+            loc.save()
+
+
+if __name__ == '__main__':
+    cleanup_keys()
+    cleanup_libvirt_cloud_locations()
